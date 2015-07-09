61b51832ade575cdb1ca7e766653d8023617b9d3
Ola Angelsmark <ola.angelsmark@ericsson.com>
2015-07-09 16:18:39 +0200

    control_api_doc: output markdown

--- calvin/utilities/control_api_doc.py
+++ calvin/utilities/control_api_doc.py
@@ -2,4 +2,16 @@
 
 from calvin.runtime.north.calvincontrol import control_api_doc
 
-print control_api_doc
+lines = control_api_doc.split("\n")
+
+block = []
+
+print "# Calvin Control API"
+for line in lines:
+    if not line and block:
+        print '- __' + block.pop(1).strip() + '__'
+        print "```\n" + "\n".join(s for s in block) + "\n```"
+        block =  []
+    elif line:
+        # same block
+        block.append(line)
