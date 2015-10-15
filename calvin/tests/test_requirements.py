af2fe8258cce252fb9d73f571acb89019551edf2
Harald Gustafsson <harald.gustafsson@gmail.com>
2015-10-15 19:03:28 +0200

    Make the first class of tests only setup runtimes once just as the second class. Also add verification that the storage works as intended before doing the actual tests, to be able to separate storage errors from the intended test objectives.

--- calvin/tests/test_requirements.py
+++ calvin/tests/test_requirements.py
@@ -40,89 +40,124 @@ except:
     import socket
     ip_addr = socket.gethostbyname(socket.gethostname())
 
+rt1 = None
+rt2 = None
+rt3 = None
+test_script_dir = None
 
 def absolute_filename(filename):
     import os.path
     return os.path.join(os.path.dirname(__file__), filename)
 
-class CalvinNodeTestBase(unittest.TestCase):
 
-    def setUp(self):
-        self.rt1, _ = dispatch_node("calvinip://%s:5000" % (ip_addr,), "http://%s:5003" % ip_addr,
+@pytest.mark.slow
+class TestDeployScript(unittest.TestCase):
+
+    @pytest.fixture(autouse=True, scope="class")
+    def setup(self, request):
+        global rt1
+        global rt2
+        global rt3
+        global test_script_dir
+        rt1, _ = dispatch_node("calvinip://%s:5000" % (ip_addr,), "http://%s:5003" % ip_addr,
              attributes={'indexed_public':
                   {'owner':{'organization': 'org.testexample', 'personOrGroup': 'testOwner1'},
                    'node_name': {'organization': 'org.testexample', 'name': 'testNode1'},
                    'address': {'country': 'SE', 'locality': 'testCity', 'street': 'testStreet', 'streetNumber': 1}}})
-
-        self.rt2, _ = dispatch_node("calvinip://%s:5001" % (ip_addr,), "http://%s:5004" % ip_addr,
+        time.sleep(1)  # Less storage operations are droped if we wait a bit
+        rt2, _ = dispatch_node("calvinip://%s:5001" % (ip_addr,), "http://%s:5004" % ip_addr,
              attributes={'indexed_public':
                   {'owner':{'organization': 'org.testexample', 'personOrGroup': 'testOwner1'},
                    'node_name': {'organization': 'org.testexample', 'name': 'testNode2'},
                    'address': {'country': 'SE', 'locality': 'testCity', 'street': 'testStreet', 'streetNumber': 1}}})
-        self.rt3, _ = dispatch_node("calvinip://%s:5002" % (ip_addr,), "http://%s:5005" % ip_addr,
+        time.sleep(1)  # Less storage operations are droped if we wait a bit
+        rt3, _ = dispatch_node("calvinip://%s:5002" % (ip_addr,), "http://%s:5005" % ip_addr,
              attributes={'indexed_public':
                   {'owner':{'organization': 'org.testexample', 'personOrGroup': 'testOwner2'},
                    'node_name': {'organization': 'org.testexample', 'name': 'testNode3'},
                    'address': {'country': 'SE', 'locality': 'testCity', 'street': 'testStreet', 'streetNumber': 2}}})
+        time.sleep(1)  # Less storage operations are droped if we wait a bit
 
-    def tearDown(self):
-        utils.quit(self.rt1)
-        utils.quit(self.rt2)
-        utils.quit(self.rt3)
+        test_script_dir = absolute_filename('scripts/')
+        request.addfinalizer(self.teardown)
+
+    def teardown(self):
+        global rt1
+        global rt2
+        global rt3
+        utils.quit(rt1)
+        utils.quit(rt2)
+        utils.quit(rt3)
         time.sleep(0.2)
         for p in multiprocessing.active_children():
             p.terminate()
         time.sleep(0.2)
 
-
-@pytest.mark.slow
-class TestDeployScript(CalvinNodeTestBase):
-
-    def setUp(self):
-        super(TestDeployScript, self).setUp()
-        self.test_script_dir = absolute_filename('scripts/')
+    def verify_storage(self):
+        global rt1
+        global rt2
+        global rt3
+        rt1_id = utils.get_node_id(rt1)
+        rt2_id = utils.get_node_id(rt2)
+        rt3_id = utils.get_node_id(rt3)
+        assert rt1_id
+        assert rt2_id
+        assert rt3_id
+        caps = utils.get_index(rt1, 'node/capabilities/calvinsys.events.timer')
+        assert set(caps['result']) >= set([rt1_id, rt2_id, rt3_id])
+        caps = utils.get_index(rt2, 'node/capabilities/calvinsys.events.timer')
+        assert set(caps['result']) >= set([rt1_id, rt2_id, rt3_id])
+        caps = utils.get_index(rt3, 'node/capabilities/calvinsys.events.timer')
+        assert set(caps['result']) >= set([rt1_id, rt2_id, rt3_id])
+        assert utils.get_index(rt2, format_index_string(['node_name', {'organization': 'org.testexample', 'name': 'testNode1'}]))
+        assert utils.get_index(rt3, format_index_string(['node_name', {'organization': 'org.testexample', 'name': 'testNode1'}]))
+        assert utils.get_index(rt1, format_index_string(['node_name', {'organization': 'org.testexample', 'name': 'testNode2'}]))
+        assert utils.get_index(rt3, format_index_string(['node_name', {'organization': 'org.testexample', 'name': 'testNode2'}]))
+        assert utils.get_index(rt1, format_index_string(['node_name', {'organization': 'org.testexample', 'name': 'testNode3'}]))
+        assert utils.get_index(rt2, format_index_string(['node_name', {'organization': 'org.testexample', 'name': 'testNode3'}]))
 
     @pytest.mark.slow
     def testDeploySimple(self):
         _log.analyze("TESTRUN", "+", {})
+        self.verify_storage()
+        
         from calvin.Tools.cscontrol import control_deploy as deploy_app
         from collections import namedtuple
         DeployArgs = namedtuple('DeployArgs', ['node', 'attr', 'script','reqs', 'check'])
-        time.sleep(2)
         args = DeployArgs(node='http://%s:5003' % ip_addr,
-                          script=open(self.test_script_dir+"test_deploy1.calvin"), attr=None,
-                                reqs=self.test_script_dir+"test_deploy1.deployjson", check=True)
+                          script=open(test_script_dir+"test_deploy1.calvin"), attr=None,
+                                reqs=test_script_dir+"test_deploy1.deployjson", check=True)
         result = {}
         try:
             result = deploy_app(args)
         except:
             raise Exception("Failed deployment of app %s, no use to verify if requirements fulfilled" % args.script.name)
         time.sleep(2)
-        actors = [utils.get_actors(self.rt1), utils.get_actors(self.rt2), utils.get_actors(self.rt3)]
+        actors = [utils.get_actors(rt1), utils.get_actors(rt2), utils.get_actors(rt3)]
         # src -> rt2, sum -> rt2, snk -> rt3
         assert result['actor_map']['test_deploy1:src'] in actors[1]
         assert result['actor_map']['test_deploy1:sum'] in actors[1]
         assert result['actor_map']['test_deploy1:snk'] in actors[2]
-        utils.delete_application(self.rt1, result['application_id'])
-        time.sleep(0.5)
+        utils.delete_application(rt1, result['application_id'])
 
     @pytest.mark.slow
     def testDeployLongActorChain(self):
         _log.analyze("TESTRUN", "+", {})
+        self.verify_storage()
+
         from calvin.Tools.cscontrol import control_deploy as deploy_app
         from collections import namedtuple
         DeployArgs = namedtuple('DeployArgs', ['node', 'attr', 'script','reqs', 'check'])
-        time.sleep(2)
         args = DeployArgs(node='http://%s:5003' % ip_addr,
-                          script=open(self.test_script_dir+"test_deploy2.calvin"), attr=None,
-                                reqs=self.test_script_dir+"test_deploy2.deployjson", check=True)
+                          script=open(test_script_dir+"test_deploy2.calvin"), attr=None,
+                                reqs=test_script_dir+"test_deploy2.deployjson", check=True)
         result = {}
         try:
             result = deploy_app(args)
         except:
             raise Exception("Failed deployment of app %s, no use to verify if requirements fulfilled" % args.script.name)
         time.sleep(2)
-        actors = [utils.get_actors(self.rt1), utils.get_actors(self.rt2), utils.get_actors(self.rt3)]
+        actors = [utils.get_actors(rt1), utils.get_actors(rt2), utils.get_actors(rt3)]
         # src -> rt1, sum[1:8] -> [rt1, rt2, rt3], snk -> rt3
         assert result['actor_map']['test_deploy2:src'] in actors[0]
         assert result['actor_map']['test_deploy2:snk'] in actors[2]
@@ -130,33 +165,32 @@ class TestDeployScript(CalvinNodeTestBase):
         sum_place = [0 if a in actors[0] else 1 if a in actors[1] else 2 if a in actors[2] else -1 for a in sum_list]
         assert not any([p==-1 for p in sum_place])
         assert all(x<=y for x, y in zip(sum_place, sum_place[1:]))
-        utils.delete_application(self.rt1, result['application_id'])
-        time.sleep(0.5)
+        utils.delete_application(rt1, result['application_id'])
 
     @pytest.mark.slow
     def testDeployComponent(self):
         _log.analyze("TESTRUN", "+", {})
+        self.verify_storage()
+
         from calvin.Tools.cscontrol import control_deploy as deploy_app
         from collections import namedtuple
         DeployArgs = namedtuple('DeployArgs', ['node', 'attr', 'script','reqs', 'check'])
-        time.sleep(2)
         args = DeployArgs(node='http://%s:5003' % ip_addr,
-                          script=open(self.test_script_dir+"test_deploy3.calvin"), attr=None,
-                                reqs=self.test_script_dir+"test_deploy3.deployjson", check=True)
+                          script=open(test_script_dir+"test_deploy3.calvin"), attr=None,
+                                reqs=test_script_dir+"test_deploy3.deployjson", check=True)
         result = {}
         try:
             result = deploy_app(args)
         except:
             raise Exception("Failed deployment of app %s, no use to verify if requirements fulfilled" % args.script.name)
         time.sleep(2)
-        actors = [utils.get_actors(self.rt1), utils.get_actors(self.rt2), utils.get_actors(self.rt3)]
+        actors = [utils.get_actors(rt1), utils.get_actors(rt2), utils.get_actors(rt3)]
         # src:(first, second) -> rt1, sum -> rt2, snk -> rt3
         assert result['actor_map']['test_deploy3:src:first'] in actors[0]
         assert result['actor_map']['test_deploy3:src:second'] in actors[0]
         assert result['actor_map']['test_deploy3:sum'] in actors[1]
         assert result['actor_map']['test_deploy3:snk'] in actors[2]
-        utils.delete_application(self.rt1, result['application_id'])
-        time.sleep(0.5)
+        utils.delete_application(rt1, result['application_id'])
 
 rt1 = None
 rt2 = None
@@ -186,12 +220,13 @@ class TestDeployShadow(unittest.TestCase):
         calvin.actorstore.store._conf.config['global']['actor_paths'] = [absolute_filename('test_store')]
         calvin.calvinsys._conf = copy.deepcopy(calvin.actorstore.store._conf)
         calvin.calvinsys._conf.config['global']['capabilities_blacklist'] = ['calvinsys.events.timer']
-
+        time.sleep(1)  # Less storage operations are droped if we wait a bit
         rt2, _ = dispatch_node("calvinip://%s:5001" % (ip_addr,), "http://%s:5004" % ip_addr,
              attributes={'indexed_public':
                   {'owner':{'organization': 'org.testexample', 'personOrGroup': 'testOwner1'},
                    'node_name': {'organization': 'org.testexample', 'name': 'testNode2'},
                    'address': {'country': 'SE', 'locality': 'testCity', 'street': 'testStreet', 'streetNumber': 1}}})
+        time.sleep(1)  # Less storage operations are droped if we wait a bit
 
         test_script_dir = absolute_filename('scripts/')
         request.addfinalizer(self.teardown)
@@ -206,16 +241,32 @@ class TestDeployShadow(unittest.TestCase):
             p.terminate()
         time.sleep(0.2)
 
+    def verify_storage(self):
+        global rt1
+        global rt2
+        rt1_id = utils.get_node_id(rt1)
+        rt2_id = utils.get_node_id(rt2)
+        assert rt1_id
+        assert rt2_id
+        caps = utils.get_index(rt1, 'node/capabilities/calvinsys.events.timer')
+        assert rt1_id in caps['result']
+        caps = utils.get_index(rt2, 'node/capabilities/calvinsys.events.timer')
+        assert rt1_id in caps['result']
+        assert utils.get_index(rt1, format_index_string(['node_name', {'organization': 'org.testexample', 'name': 'testNode2'}]))
+        assert utils.get_index(rt2, format_index_string(['node_name', {'organization': 'org.testexample', 'name': 'testNode1'}]))
+
     @pytest.mark.slow
     def testDeployShadow(self):
         _log.analyze("TESTRUN", "+", {})
         global rt1
         global rt2
         global test_script_dir
+
+        self.verify_storage()
+
         from calvin.Tools.cscontrol import control_deploy as deploy_app
         from collections import namedtuple
         DeployArgs = namedtuple('DeployArgs', ['node', 'attr', 'script','reqs', 'check'])
-        time.sleep(2)
         args = DeployArgs(node='http://%s:5003' % ip_addr,
                           script=open(test_script_dir+"test_shadow1.calvin"), attr=None,
                                 reqs=test_script_dir+"test_shadow1.deployjson", check=False)
@@ -226,10 +277,6 @@ class TestDeployShadow(unittest.TestCase):
             raise Exception("Failed deployment of app %s, no use to verify if requirements fulfilled" % args.script.name)
         time.sleep(2)
 
-        # To verify storage is working
-        node2 = utils.get_index(rt1, format_index_string(['node_name', {'organization': 'org.testexample', 'name': 'testNode2'}]))
-        assert node2
-
         actors = [utils.get_actors(rt1), utils.get_actors(rt2)]
         # src -> rt1, sum -> rt2, snk -> rt1
         assert result['actor_map']['test_shadow1:src'] in actors[0]
@@ -239,7 +286,6 @@ class TestDeployShadow(unittest.TestCase):
         actual = utils.report(rt1, result['actor_map']['test_shadow1:snk'])
         assert len(actual) > 5
         utils.delete_application(rt1, result['application_id'])
-        time.sleep(0.5)
 
     @pytest.mark.slow
     def testDeployRequiresShadow(self):
@@ -247,10 +293,12 @@ class TestDeployShadow(unittest.TestCase):
         global rt1
         global rt2
         global test_script_dir
+
+        self.verify_storage()
+
         from calvin.Tools.cscontrol import control_deploy as deploy_app
         from collections import namedtuple
         DeployArgs = namedtuple('DeployArgs', ['node', 'attr', 'script','reqs', 'check'])
-        time.sleep(2)
         args = DeployArgs(node='http://%s:5003' % ip_addr,
                           script=open(test_script_dir+"test_shadow1.calvin"), attr=None,
                                 reqs=test_script_dir+"test_shadow2.deployjson", check=False)
@@ -261,10 +309,6 @@ class TestDeployShadow(unittest.TestCase):
             raise Exception("Failed deployment of app %s, no use to verify if requirements fulfilled" % args.script.name)
         time.sleep(2)
 
-        # To verify storage is working
-        node2 = utils.get_index(rt1, format_index_string(['node_name', {'organization': 'org.testexample', 'name': 'testNode2'}]))
-        assert node2
-
         actors = [utils.get_actors(rt1), utils.get_actors(rt2)]
         # src -> rt1, sum -> rt2, snk -> rt1
         assert result['actor_map']['test_shadow1:src'] in actors[0]
@@ -274,4 +318,3 @@ class TestDeployShadow(unittest.TestCase):
         actual = utils.report(rt1, result['actor_map']['test_shadow1:snk'])
         assert len(actual) > 5
         utils.delete_application(rt1, result['application_id'])
-        time.sleep(0.5)
