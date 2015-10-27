17a93364fef9f4e0079c3c6735d3f96a9e083ed0
Ola Angelsmark <ola.angelsmark@ericsson.com>
2015-10-27 10:38:03 +0100

    Allow deployment of non-local components

--- calvin/csparser/analyzer.py
+++ calvin/csparser/analyzer.py
@@ -115,7 +115,7 @@ class Analyzer(object):
         if self.verify and not found:
             msg = 'Actor "{}" not found.'.format(actor_type)
             raise Exception(msg)
-        return info, True
+        return info, is_actor
 
     def add_connection(self, src_actor_port, dst_actor_port):
         if type(dst_actor_port) is list:
