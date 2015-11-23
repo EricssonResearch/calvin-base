34b7a43c0a447bc89f73ee2067654f0500946db5
Ola Angelsmark <ola.angelsmark@ericsson.com>
2015-11-23 09:49:03 +0100

    License and minor style issue

--- calvin/actorstore/systemactors/media/MediaPlayer.py
+++ calvin/actorstore/systemactors/media/MediaPlayer.py
@@ -1,4 +1,20 @@
-from calvin.actor.actor import Actor, ActionResult, manage, condition
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015 Ericsson AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from calvin.actor.actor import Actor, ActionResult, manage, condition, guard
 
 
 class MediaPlayer(Actor):
@@ -28,12 +44,16 @@ class MediaPlayer(Actor):
     def will_end(self):
         self.player.close()
 
-    @condition(action_input=('play',))
+    @condition(['play'], [])
+    @guard(lambda _, play: play)
     def play(self, play):
-        if play:
-            self.player.play(self.media_file)
+        self.player.play(self.media_file)
         return ActionResult(production=())
 
-    action_priority = (play, )
+    @condition(['play'], [])
+    @guard(lambda _, play: not play)
+    def ignore(self, _):
+        return ActionResult(production=())
+
+    action_priority = (ignore, play, )
     requires =  ['calvinsys.media.mediaplayer']
-    
