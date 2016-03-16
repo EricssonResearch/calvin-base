88e4a223fefed27f77b7c6bd7cef61b23187a449
Update path <eperspe@r2d2.local>
2016-03-16 13:19:28 +0100

    Track parents in all nodes

--- calvin/csparser/astnode.py
+++ calvin/csparser/astnode.py
@@ -8,7 +8,29 @@ class Node(object):
     """Base class for all nodes in CS AST"""
     def __init__(self):
         super(Node, self).__init__()
+        self.parent = None
         self.children = []
+
+    def is_leaf(self):
+        return self.children is None
+
+    def add_child(self, child):
+        if self.is_leaf():
+            raise Exception("Can't add children to leaf node {}".format(self))
+        child.parent = self
+        self.children.append(child)
+
+    def add_children(self, children):
+        for child in children:
+            self.add_child(child)
+
+    def remove_child(self, child):
+        if self.is_leaf():
+            raise Exception("Can't remove children from leaf node {}".format(self))
+        if child in self.children:
+            self.children.remove(child)
+            child.parent = None
+
 
     def __str__(self):
         if self._verbose_desc:
@@ -20,18 +42,20 @@ class Constant(Node):
     """docstring for ConstNode"""
     def __init__(self, ident, arg):
         super(Constant, self).__init__()
-        self.children = [ident, arg]
+        self.add_children([ident, arg])
 
 class Id(Node):
     """docstring for IdNode"""
     def __init__(self, ident):
         super(Id, self).__init__()
+        self.children = None
         self.ident = ident
 
 class Value(Node):
     """docstring for ValueNode"""
     def __init__(self, value):
         super(Value, self).__init__()
+        self.children = None
         self.value = value
 
 class Assignment(Node):
@@ -40,7 +64,7 @@ class Assignment(Node):
         super(Assignment, self).__init__()
         self.ident = ident
         self.actor_type = actor_type
-        self.children = args
+        self.add_children(args)
 
     def __copy__(self):
         return Assignment(copy(self.ident), copy(self.actor_type), deepcopy(self.children))
@@ -49,7 +73,7 @@ class NamedArg(Node):
     """docstring for ConstNode"""
     def __init__(self, ident, arg):
         super(NamedArg, self).__init__()
-        self.children = [ident, arg]
+        self.add_children([ident, arg])
 
     def __copy__(self, memo):
         print "NamedArg.copy"
@@ -59,11 +83,14 @@ class Link(Node):
     """docstring for LinkNode"""
     def __init__(self, outport, inport):
         super(Link, self).__init__()
-        self.children = [outport, inport]
+        self.add_children([outport, inport])
 
     def __copy__(self):
         print "Link", self.outport, copy(self.outport)
         return Link(copy(self.outport), copy(self.inport))
+
+    def remove_child(self, child):
+        raise Exception("Can't remove child from {}".format(self))
 
     @property
     def outport(self):
@@ -71,6 +98,8 @@ class Link(Node):
 
     @outport.setter
     def outport(self, value):
+        value.parent = self
+        self.outport.parent = None
         self.children[0] = value
 
     @property
@@ -79,6 +108,8 @@ class Link(Node):
 
     @inport.setter
     def inport(self, value):
+        value.parent = self
+        self.inport.parent = None
         self.children[1] = value
 
 
@@ -86,6 +117,7 @@ class Port(Node):
     """docstring for LinkNode"""
     def __init__(self, actor, port):
         super(Port, self).__init__()
+        self.children = None
         self.actor = actor
         self.port = port
 
@@ -93,26 +125,20 @@ class ImplicitPort(Node):
     """docstring for ImplicitPortNode"""
     def __init__(self, arg):
         super(ImplicitPort, self).__init__()
-        self.children = [arg]
+        self.add_child(arg)
 
 class InternalPort(Node):
     """docstring for InternalPortNode"""
     def __init__(self, port):
         super(InternalPort, self).__init__()
+        self.children = None
         self.port = port
 
 class Block(Node):
     """docstring for ComponentNode"""
     def __init__(self, program = None):
         super(Block, self).__init__()
-        self.children = program or []
-
-    def append(self, node):
-        index = 0 if type(node) is Assignment else len(self.children)
-        self.children.insert(index, node)
-
-    def remove(self, node):
-        self.children.remove(node)
+        self.add_children(program or [])
 
     def __copy__(self):
         return Block(deepcopy(self.children))
@@ -127,12 +153,13 @@ class Component(Node):
         self.inports = inports
         self.outports = outports
         self.docstring = docstring
-        self.children = [Block(program)]
+        self.add_child = Block(program)
 
 if __name__ == '__main__':
     # Node._verbose_desc = True
-    l = Link(Port('foo', 'out'), Port('bar', 'in'))
-    a = Assignment('foo', 'std.Source', [NamedArg('n', 10), NamedArg('str', 'hello')])
+    p = Port('foo', 'out')
+    l = Link(p, Port('bar', 'in'))
+    a = Assignment('foo', 'std.Source', [NamedArg(Id('n'), Value(10)), NamedArg(Id('str'), Value('hello'))])
     print l, l.outport, l.inport
     lc = copy(l)
     print lc, lc.outport, lc.inport
@@ -142,5 +169,11 @@ if __name__ == '__main__':
     print ac, ac.ident, ac.actor_type, id(ac.children), ac.children[0], ac.children[1]
     ac.children[0].children[1] = 42
     print a.children[0].children[1], ac.children[0].children[1]
+
+    n = p
+    while n:
+        print n
+        n = n.parent
+
 
 
