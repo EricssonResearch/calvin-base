a1ce4de5fbd0a3333fc2d4b1bff758c3d8f46836
Ola Angelsmark <ola.angelsmark@ericsson.com>
2016-05-18 13:03:03 +0200

    install: add ez_setup

--- extras/install/ubuntu-install.sh
+++ extras/install/ubuntu-install.sh
@@ -2,6 +2,7 @@
 apt-get update
 apt-get install -y python python-dev build-essential git libssl-dev libffi-dev
 apt-get remove python-pip
+curl https://bootstrap.pypa.io/ez_setup.py -o - | python
 easy_install -U pip
 git clone https://www.github.com/EricssonResearch/calvin-base
 cd calvin-base
