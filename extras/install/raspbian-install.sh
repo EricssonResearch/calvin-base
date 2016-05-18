a4b62254a801ba5563374322fb653e019a79f902
Ola Angelsmark <ola.angelsmark@ericsson.com>
2016-05-18 12:47:14 +0200

    Raspbian: install script

---
+++ extras/install/raspbian-install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+apt-get update
+apt-get install -y python python-dev build-essential git libssl-dev libffi-dev
+apt-get remove python-pip
+easy_install -U pip
+git clone https://www.github.com/EricssonResearch/calvin-base
+cd calvin-base
+pip install -r requirements.txt -r test-requirements.txt
+pip install --upgrade pyasn1 setuptools six
+pip install -e .
