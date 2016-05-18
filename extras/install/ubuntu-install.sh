138917df52df55e756347ee728ddf1fe59722b2d
Ola Angelsmark <ola.angelsmark@ericsson.com>
2016-05-18 14:18:20 +0200

    install: select branch when installing

--- extras/install/ubuntu-install.sh
+++ extras/install/ubuntu-install.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
+
+while getopts "b:" n
+do
+	case $n in
+	  b) branch="$OPTARG";;
+	  ?) printf "Usage: %s [-b master/develop]\n" $0
+	     exit 0;;
+	esac
+done
+
+if [ -z "$branch" ]; then
+	branch="master"
+fi
+shift $(($OPTIND - 1))
+
 apt-get update
 apt-get install -y python python-dev build-essential git libssl-dev libffi-dev
 apt-get remove python-pip
 curl https://bootstrap.pypa.io/ez_setup.py -o - | python
 easy_install -U pip
-git clone https://www.github.com/EricssonResearch/calvin-base
+git clone -b $branch https://www.github.com/EricssonResearch/calvin-base
 cd calvin-base
 pip install -r requirements.txt -r test-requirements.txt
 pip install --upgrade pyasn1 setuptools six
