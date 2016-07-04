926fc55d4f5eb39f78b94a8e71b1cf51f7ed52f5
Per Persson <persquare@mac.com>
2016-07-04 17:12:51 +0200

    Refactoring, no change in semantics.

--- calvin/csparser/cscompile.py
+++ calvin/csparser/cscompile.py
@@ -1,7 +1,7 @@
 
 import os
 from parser import calvin_parser
-from codegen import CodeGen
+from codegen import generate_app_info
 from calvin.utilities.security import Security, security_enabled
 from calvin.utilities.calvin_callback import CalvinCB
 from calvin.utilities.calvinlogger import get_logger
@@ -10,88 +10,89 @@ _log = get_logger(__name__)
 
 
 def compile_script(source_text, filename, credentials=None, verify=True, node=None, cb=None):
-    # Steps taken:
-    # 1) Authenticate subject, verify signature and check security policy if security is enabled
-    # 2) parser .calvin file -> IR. May produce syntax errors/warnings
-    # 3) checker IR -> IR. May produce syntax errors/warnings
-    # 4) analyzer IR -> app. Should not fail. Sets 'valid' property of IR to True/False
+    """
+    Compile a script and return a tuple (deployable, errors, warnings).
+
+    'credentials' are optional security credentials(?)
+    'verify' is deprecated and will be removed
+    'node' is the runtime performing security check(?)
+    'cb' is a CalvinCB callback
+
+    N.B. If callback 'cb' is given, this method calls cb(deployable, errors, warnings) and returns None
+    """
+
+    def _exit_with_error(err, callback):
+        """
+        Return with proper tuple unless callback given.
+        In that case call callback and return None
+        """
+        reply = ({}, [err], [])
+        if not callback:
+            return reply
+        callback(*reply)
 
 
     def _compile_cont1(source_text, filename, verify, authentication_decision, security, org_cb=None, content=None):
-        deployable = {'valid': False, 'actors': {}, 'connections': {}}
-        errors = [] # TODO: fill in something meaningful
-        warnings = []
         if not authentication_decision:
             _log.error("Authentication failed")
             # This error reason is detected in calvin control and gives proper REST response
-            errors.append({'reason': "401: UNAUTHORIZED", 'line': None, 'col': None})
-            if org_cb:
-                org_cb(deployable, errors, warnings)
-                return
-            else:
-                return deployable, errors, warnings
+            _exit_with_error({'reason': "401: UNAUTHORIZED", 'line': None, 'col': None}, org_cb)
 
         verified, signer = security.verify_signature_content(content, "application")
         if not verified:
             # Verification not OK if sign or cert not OK.
             _log.error("Failed application verification")
             # This error reason is detected in calvin control and gives proper REST response
-            errors.append({'reason': "401: UNAUTHORIZED", 'line': None, 'col': None})
-            if org_cb:
-                org_cb(deployable, errors, warnings)
-                return
-            else:
-                return deployable, errors, warnings
-        security.check_security_policy(CalvinCB(_compile_cont2, source_text, filename, verify,
-                                           security=security, org_cb=org_cb), "application", signer=signer)
+            _exit_with_error({'reason': "401: UNAUTHORIZED", 'line': None, 'col': None}, org_cb)
+
+        security.check_security_policy(
+            CalvinCB(_compile_cont2, source_text, filename, verify, security=security, org_cb=org_cb),
+            "application",
+            signer=signer
+        )
 
     def _compile_cont2(source_text, filename, verify, access_decision, security=None, org_cb=None):
-        deployable = {'valid': False, 'actors': {}, 'connections': {}}
-        errors = [] # TODO: fill in something meaningful
-        warnings = []
         if not access_decision:
             _log.error("Access denied")
             # This error reason is detected in calvin control and gives proper REST response
-            errors.append({'reason': "401: UNAUTHORIZED", 'line': None, 'col': None})
-            if org_cb:
-                org_cb(deployable, errors, warnings)
-                return
-            else:
-                return deployable, errors, warnings
-        _log.debug("Parsing...")
+            _exit_with_error({'reason': "401: UNAUTHORIZED", 'line': None, 'col': None}, org_cb)
+
         ir, errors, warnings = calvin_parser(source_text, filename)
-        _log.debug("Parsed %s, %s, %s" % (ir, errors, warnings))
-
         app_name = os.path.splitext(os.path.basename(filename))[0]
-        codegen = CodeGen(ir, app_name, verify=verify)
-        codegen.run()
-        deployable = codegen.app_info
-        errors.extend([issue for issue in codegen.issues if issue['type'] == 'error'])
-        warnings.extend([issue for issue in codegen.issues if issue['type'] == 'warning'])
+        deployable, issues = generate_app_info(ir, app_name, verify=verify)
+        errors = [issue for issue in issues if issue['type'] == 'error']
+        warnings = [issue for issue in issues if issue['type'] == 'warning']
 
-
-        _log.debug("Compiled %s, %s, %s" % (deployable, errors, warnings))
         if org_cb:
             org_cb(deployable, errors, warnings, security=security)
         else:
             return deployable, errors, warnings
 
+    #
+    # Actual code for compile_script
+    #
 
-    content = None
     if credentials:
         content = Security.verify_signature_get_files(filename, skip_file=True)
         # content is ALWAYS a dict if skip_file is True
         content['file'] = source_text
-
-    deployable = {'valid': False, 'actors': {}, 'connections': {}}
-    errors = [] # TODO: fill in something meaningful
-    warnings = []
-    if node is not None and security_enabled():
-        sec = Security(node)
-        sec.authenticate_subject(credentials, callback=CalvinCB(_compile_cont1, source_text,
-                                                filename, verify, security=sec, org_cb=cb, content=content))
     else:
-        if cb:
-            _compile_cont2(source_text, filename, verify, True, org_cb=cb)
-        else:
-            return _compile_cont2(source_text, filename, verify, True)
+        content = None
+
+    # FIXME: if node is None we bypass security even if enabled. Is that the intention?
+    if node is not None and security_enabled():
+        # FIXME: If cb is None, we will return from this method with None instead of a tuple, failing silently
+        sec = Security(node)
+        sec.authenticate_subject(
+            credentials,
+            callback=CalvinCB(_compile_cont1, source_text, filename, verify, security=sec, org_cb=cb, content=content)
+        )
+        return
+
+    #
+    # We get here if node is None, or security is disabled
+    #
+    if cb:
+        _compile_cont2(source_text, filename, verify, access_decision=True, security=None, org_cb=cb)
+    else:
+        return _compile_cont2(source_text, filename, verify, access_decision=True, security=None, org_cb=None)
