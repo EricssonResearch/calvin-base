a5b73f25bf73569bf2d98881dd4ca7c1253c6cb5
Per Persson <persquare@mac.com>
2016-07-05 16:53:18 +0200

    Reviving csviz, but currently it cannot show component internals.

---
+++ calvin/csparser/visualize.py
@@ -0,0 +1,208 @@
+import visitor
+import astnode as ast
+from codegen import CodeGen, query
+from parser import calvin_parser
+from calvin.actorstore.store import DocumentationStore
+
+_docstore = DocumentationStore()
+
+def _refname(name):
+   return name.replace(':', '_')
+
+def _lookup_definition(actor_type, root):
+    if '.' in actor_type:
+        doc = _docstore.help_raw(actor_type)
+        if not doc['name']:
+            return ([], [], '')
+        t = doc['type']
+
+        inports = [p for p,_ in doc['inputs']]
+        outports = [p for p,_ in doc['outputs']]
+    else:
+        t = 'component'
+        comps = query(root, kind=ast.Component, attributes={'name':actor_type}, maxdepth=2)
+        if not comps:
+            return ([], [], '')
+
+        inports, outports = comps[0].inports, comps[0].outports
+
+    return (inports, outports, t)
+
+#
+# Implement _vis_xxx for each node type with the following, and ...
+#
+def _viz_op(self):
+    return "{}:{}_out:e".format(_refname(self.actor), self.port)
+
+def _viz_ip(self):
+    return "{}:{}_in:w".format(_refname(self.actor), self.port)
+
+def _viz_iop(self):
+    return "{}_out:e".format(self.port)
+
+def _viz_iip(self):
+    return "{}_in:w".format(self.port)
+
+def _viz_implp(self):
+    return "{}".format(self.arg)
+
+def _viz_value(self):
+    return "{}".format(self.value)
+
+def _viz_id(self):
+    return "{}".format(self.ident)
+
+def _viz_actor(self):
+    # ident, actor_type, args
+    root = self
+    while root.parent:
+        root = root.parent
+    _inports, _outports, _type = _lookup_definition(self.actor_type, root)
+    inlen = len(_inports)
+    outlen = len(_outports)
+    portrows = max(inlen, outlen)
+    inports = _inports + ['']*(portrows - inlen)
+    outports = _outports + ['']*(portrows - outlen)
+    hdrcolor = {
+        'actor': 'lightblue',
+        'component': 'lightyellow'
+    }.get(_type, 'tomato')
+
+    lines = []
+    lines.append('{} [label=<'.format(_refname(self.ident)))
+    lines.append('<TABLE BORDER="1" CELLBORDER="0" CELLSPACING="0" CELLPADDING="1">')
+    # Name
+    lines.append('<TR><TD bgcolor="{1}" COLSPAN="3">{0}</TD></TR>'.format(self.ident, hdrcolor))
+    # Class
+    lines.append('<TR><TD COLSPAN="3">{}</TD></TR>'.format(self.actor_type))
+    is_first=True
+    for inport, outport in zip(inports, outports):
+        inref = ' bgcolor="lightgrey" PORT="{}_in"'.format(inport) if inport else ''
+        outref = ' bgcolor="lightgrey" PORT="{}_out"'.format(outport) if outport else ''
+        if is_first:
+            is_first = False
+            middle = '<TD ROWSPAN="{}">    </TD>'.format(portrows)
+        else:
+            middle = ''
+        lines.append('<TR><TD{0} align="left">{1}</TD>{4}<TD{2} align="right">{3}</TD></TR>'.format(inref, inport, outref, outport, middle))
+    lines.append('</TABLE>>];')
+
+    return '\n'.join(lines)
+
+#
+# ... monkey patch __str__ of the various node types
+#
+ast.OutPort.__str__ = _viz_op
+ast.InPort.__str__ = _viz_ip
+ast.InternalOutPort.__str__ = _viz_iop
+ast.InternalInPort.__str__ = _viz_iip
+ast.ImplicitPort.__str__ = _viz_implp
+ast.Value.__str__ = _viz_value
+ast.Id.__str__ = _viz_id
+ast.Assignment.__str__ = _viz_actor
+
+
+
+class Visualize(object):
+   """docstring for VizPrinter"""
+   def __init__(self):
+       super(Visualize, self).__init__()
+       self.expand_components = False
+
+   def add(self, stmt):
+       self.statements.append(stmt)
+
+   def init(self, root):
+       self.root = root
+       self.statements = []
+
+   def process(self, node):
+       try:
+           self.init(node)
+           self.add('digraph structs { node [shape=plaintext]; rankdir=LR;')
+           # self.add('digraph structs { node [shape=plaintext];')
+           self.visit(node)
+           self.add('}')
+           return "\n".join(self.statements)
+       except:
+           print self.statements
+
+   @visitor.on('node')
+   def visit(self, node):
+       pass
+
+   @visitor.when(ast.Node)
+   def visit(self, node):
+       if node.children:
+           map(self.visit, node.children)
+
+   # FIXME: Make subgraph
+   @visitor.when(ast.Component)
+   def visit(self, node):
+       if self.expand_components:
+           self.add('subgraph { label=%s;' % node.name)
+           for p in node.inports:
+               self.add('{0}_out [shape="cds" style="filled" fillcolor="lightgrey" label="{0}"]'.format(p))
+           for p in node.outports:
+               self.add('{0}_in [shape="cds" style="filled" fillcolor="lightgrey" label="{0}"]'.format(p))
+           if node.children:
+               # print node.children
+               map(self.visit, node.children)
+           self.add('}')
+
+   @visitor.when(ast.Assignment)
+   def visit(self, node):
+       self.add(str(node))
+
+
+   @visitor.when(ast.Link)
+   def visit(self, node):
+       res = "{} -> {};".format(node.outport, node.inport)
+       self.add(res)
+
+
+def visualize_script(source_text):
+    ast, issues, _ = calvin_parser(source_text)
+    v = Visualize()
+    return v.process(ast), issues
+
+
+def visualize_deployment(source_text):
+    ast_root, issues, _ = calvin_parser(source_text)
+    cg = CodeGen(ast_root, 'temp')
+    cg.run()
+    issues.extend(cg.issues)
+    v = Visualize()
+    return v.process(cg.root), issues
+
+def visualize_component(source_text, name):
+    # FIXME: Not working
+    ast_root, issues, _ = calvin_parser(source_text)
+    cg = CodeGen(ast_root, 'temp')
+    comps, cg_issues = cg.export_components([name])
+    issues.extend(cg_issues)
+    if not comps:
+        return "digraph structs {ERROR}", issues
+    v = Visualize()
+    res = v.process(comps[0])
+    return res, issues
+
+
+
+
+
+if __name__ == '__main__':
+    from parser import calvin_parser
+
+    source_text = """
+    /* Actors */
+    src : std.CountTimer()
+    snk : io.Print()
+    /* Connections */
+    src.integer > snk.token
+    """
+
+    ast, issues, _ = calvin_parser(source_text)
+
+    v = Visualize()
+    print v.process(ast)
