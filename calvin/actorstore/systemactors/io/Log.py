0771f730a854be125d1e8130cda1d330028eebb0
Ola Angelsmark <ola.angelsmark@ericsson.com>
2016-07-06 14:03:50 +0200

    actor: added log actor for tests

---
+++ calvin/actorstore/systemactors/io/Log.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2016 Ericsson AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from calvin.actor.actor import Actor, ActionResult, manage, condition
+from calvin.utilities import calvinlogger
+_log = calvinlogger.get_logger(__name__)
+
+
+class Log(Actor):
+    """
+    Write data to calvin log using specified loglevel. 
+    Supported loglevels: INFO, WARNING, ERROR
+
+    Input:
+      data : data to be logger
+    """
+
+    def exception_handler(self, action, args, context):
+        # Check args to verify that it is EOSToken
+        return action(self, *args)
+
+    @manage(['loglevel'])
+    def init(self, loglevel):
+        self.loglevel = loglevel
+        self.setup()
+        
+    def setup(self):
+        if self.loglevel == "INFO":
+            self.logger = _log.info
+        elif self.loglevel == "WARNING":
+            self.logger = _log.warning
+        elif self.loglevel == "ERROR":
+            self.logger = _log.error
+        else :
+            self.logger = _log.info
+
+    def will_migrate(self):
+        self.logger(" -- migrating")
+    
+    def did_migrate(self):
+        self.setup()
+        self.logger(" -- finished migrating")
+    
+    @condition(action_input=['data'])
+    def action(self, data):
+        self.logger("{}".format(data))
+        return ActionResult()
+
+    action_priority = (action, )
+
