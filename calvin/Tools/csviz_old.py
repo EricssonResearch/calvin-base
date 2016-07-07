04464ff1162eb276e41b31c26d43333fe2e5d8b9
Per Persson <persquare@mac.com>
2016-07-07 11:25:00 +0200

    Adding issuetracker too

---
+++ calvin/Tools/csviz_old.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015 Ericsson AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import sys
+import textwrap
+import argparse
+from calvin.actorstore.store import DocumentationStore
+from calvin.Tools import cscompiler
+from calvin.csparser.parser import calvin_parser
+
+
+def _refname(name):
+    return "" if name == '.' else name.replace(':', '_')
+
+class Viz(object):
+    """docstring for Viz"""
+    def __init__(self):
+        super(Viz, self).__init__()
+
+    def __str__(self):
+        raise Exception("Direct subclass MUST implement __str__")
+
+    def render(self):
+        return str(self)
+
+class LinkViz(Viz):
+    """docstring for LinkViz"""
+    def __init__(self, link):
+        super(LinkViz, self).__init__()
+        link['src'] = _refname(link['src'])
+        link['dst'] = _refname(link['dst'])
+        self.link = link
+
+    def __str__(self):
+        if not self.link['src']:
+            return '{src_port}_in:e -> {dst}:{dst_port}_in:w [arrowhead="none"];'.format(**self.link)
+        elif not self.link['dst']:
+            return '{src}:{src_port}_out:e -> {dst_port}_out:w [arrowhead="none"];'.format(**self.link)
+        else:
+            return '{src}:{src_port}_out:e -> {dst}:{dst_port}_in:w;'.format(**self.link)
+
+class ActorViz(Viz):
+    """docstring for ActorViz"""
+
+    docstore = DocumentationStore()
+
+    def __init__(self, name, actor_type, args, **dummy):
+        super(ActorViz, self).__init__()
+        self.type_color = 'lightblue'
+        self.name = name
+        self.args = args
+        self.actor_type = actor_type
+        doc = self.docstore.help_raw(actor_type)
+        self.set_ports(doc)
+
+    def set_ports(self, doc):
+        inports = [p for p,_ in doc['inputs']]
+        outports = [p for p,_ in doc['outputs']]
+        inlen = len(inports)
+        outlen = len(outports)
+        self.portrows = max(inlen, outlen)
+        self.inports = inports + ['']*(self.portrows - inlen)
+        self.outports = outports + ['']*(self.portrows - outlen)
+
+
+    def __str__(self):
+        lines = []
+        lines.append('{0} [label=<'.format(_refname(self.name)))
+        lines.append('<TABLE BORDER="1" CELLBORDER="0" CELLSPACING="0" CELLPADDING="1">')
+        # Name
+        lines.append('<TR><TD bgcolor="{1}" COLSPAN="3">{0}</TD></TR>'.format(self.name, self.type_color))
+        # Class
+        lines.append('<TR><TD COLSPAN="3">{0}</TD></TR>'.format(self.actor_type))
+        is_first=True
+        for inport, outport in zip(self.inports, self.outports):
+            inref = ' bgcolor="lightgrey" PORT="{0}_in"'.format(inport) if inport else ''
+            outref = ' bgcolor="lightgrey" PORT="{0}_out"'.format(outport) if outport else ''
+            if is_first:
+                is_first = False
+                middle = '<TD ROWSPAN="{0}">    </TD>'.format(self.portrows)
+            else:
+                middle = ''
+            lines.append('<TR><TD{0} align="left">{1}</TD>{4}<TD{2} align="right">{3}</TD></TR>'.format(inref, inport, outref, outport, middle))
+        lines.append('</TABLE>>];')
+
+        return '\n'.join(lines)
+
+
+class PadViz(Viz):
+    """docstring for PadViz"""
+    def __init__(self, padname, direction):
+        super(PadViz, self).__init__()
+        self.padname = padname
+        self.direction = direction
+
+    def __str__(self):
+        return '{0}_{1} [shape="cds" style="filled" fillcolor="lightgrey" label="{0}"];'.format(self.padname, self.direction)
+
+
+class CompViz(ActorViz):
+    """docstring for CompViz"""
+    def __init__(self, name, comp_type, comp_def):
+        self.type_color = 'lightyellow'
+        self.name = name
+        self.args = comp_def['arg_identifiers']
+        self.actor_type = comp_type
+        doc = self.docstore.component_docs('.'+comp_type, comp_def)
+        self.set_ports(doc)
+
+
+class AppViz(Viz):
+    """docstring for AppViz"""
+    def __init__(self, deployable):
+        super(AppViz, self).__init__()
+        self.actors = [ActorViz(name, **args) for name, args in deployable['actors'].iteritems()]
+        self.links = []
+        for src, dstlist in deployable['connections'].iteritems():
+            _src, _src_port = src.split('.')
+            for dst in dstlist:
+                _dst, _dst_port = dst.split('.')
+                link = {'src':_src, 'src_port':_src_port, 'dst':_dst, 'dst_port':_dst_port}
+                self.links.append(LinkViz(link))
+        self.components = []
+
+    def __str__(self):
+        viz = [str(v) for v in self.actors + self.links + self.components]
+        return '\n'.join(viz)
+
+    def render(self):
+        return 'digraph structs {{ node [shape=plaintext]; rankdir=LR;\n{0}\n}}'.format(str(self))
+
+
+class ScriptViz(AppViz):
+    """docstring for ScriptViz"""
+    def __init__(self, ir):
+        comp_defs = ir['components'] if 'components' in ir else {}
+        self.actors = [ActorViz(name, **args) for name, args in ir['structure']['actors'].iteritems() if '.' in args['actor_type']]
+        self.links = [LinkViz(link) for link in ir['structure']['connections'] if link['src'] and link['dst']]
+        self.components = [CompViz(name, args['actor_type'], comp_defs[args['actor_type']]) for name, args in ir['structure']['actors'].iteritems() if args['actor_type'] in comp_defs]
+
+
+class CompInternalsViz(ScriptViz):
+    """docstring for CompInternalsViz"""
+    def __init__(self, comp_def):
+        super(CompInternalsViz, self).__init__(comp_def)
+        self.name = comp_def['name']
+        self.inpads = [PadViz(p, 'in') for p in comp_def['inports']]
+        self.outpads = [PadViz(p, 'out') for p in comp_def['outports']]
+        self.padlinks = [LinkViz(link) for link in comp_def['structure']['connections'] if link['src'] == '.' or link['dst'] == '.']
+
+    def render(self):
+        viz = ['digraph comp { node [shape=plaintext]; rankdir=LR;']
+        # Declare "pads"
+        viz += ['subgraph inpads { rank="source";']
+        viz += [str(v) for v in self.inpads]
+        viz.append('}')
+        viz += ['subgraph outpads { rank="sink";']
+        viz += [str(v) for v in self.outpads]
+        viz.append('}')
+        viz += ['subgraph cluster_comp {{ label="{0}";'.format(self.name)]
+        # Component structure (same as any script)
+        viz.append(str(self))
+        viz.append('}')
+        # Links to component ports
+        viz += [str(v) for v in self.padlinks]
+        viz.append('}')
+        return '\n'.join(viz)
+
+
+def visualize_deployment(filename):
+    deployable, errors, warnings = cscompiler.compile_file(filename)
+    return AppViz(deployable).render()
+
+
+def visualize_script(filename):
+    with open(filename, 'r') as f:
+        source_text = f.read()
+        ir, errors, warnings = calvin_parser(source_text)
+        return ScriptViz(ir).render()
+
+def visualize_component_internals(filename, component):
+    with open(filename, 'r') as f:
+        source_text = f.read()
+        ir, errors, warnings = calvin_parser(source_text)
+        if component in ir['components']:
+            comp_def = ir['components'][component]
+            return CompInternalsViz(comp_def).render()
+
+def main():
+    long_description = """
+    Generate a DOT output for use with GraphViz to generate a vizualization
+    of the Calvin application graph.
+
+    Typical usage would be something like (Linux):
+    csviz --script foo.calvin | dot -Tpdf | pdfviewer -
+    or (Mac OS X):
+    csviz --script foo.calvin | dot -Tpdf | open -f -a Preview
+    """
+
+    argparser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent(long_description)
+    )
+    argparser.add_argument('--script', type=str, required=True,
+                           help='script file to visualize.')
+    group = argparser.add_mutually_exclusive_group()
+    group.add_argument('--deployment', action='store_true',
+                       help='expand all components into constituent parts.')
+    group.add_argument('--component', type=str,
+                       help='show internals of a component in script.')
+
+    args = argparser.parse_args()
+
+    exit_val = 0
+    try:
+        if args.deployment:
+            res = visualize_deployment(args.script)
+        elif args.component:
+            res = visualize_component_internals(args.script, args.component)
+        else:
+            res = visualize_script(args.script)
+        print(res)
+    except Exception as e:
+        print e
+        exit_val = 1
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ calvin/utilities/issuetracker.py
@@ -0,0 +1,285 @@
+import sys
+import string
+
+class IssueFormatter(string.Formatter):
+    """
+    Custom string formatter, adds
+        u : uppercase
+        l : lowercase
+        c : capitalize
+    to string formatting, e.g.
+        issue_formatter.format("{foo!c}", foo="hello world!")
+    returns "HELLO WORLD!"
+    """
+
+    # http://stackoverflow.com/q/17848202/1007047
+    # http://stackoverflow.com/q/21664318/1007047
+
+    def __init__(self):
+        super(IssueFormatter, self).__init__()
+
+    def parse(self, s):
+        position = 0
+        for lit, name, spec, conv in super(IssueFormatter, self).parse(s):
+            if not name:
+                name = str(position)
+                position += 1
+            yield lit, name, spec, conv
+
+    def convert_field(self, value, conversion):
+        if conversion == 'c':
+            return value.capitalize()
+        elif conversion == 'u':
+            return value.upper()
+        elif conversion == 'l':
+            return value.lower()
+        return super(IssueFormatter, self).convert_field(value, conversion)
+
+
+
+class IssueTracker(object):
+    """
+    A pretty generic issue tracker that can sort, format, and report back issues.
+
+    An issue is a dict with information, guaranteed to have keys, 'type' and 'reason',
+    where value of 'type' is either 'error' or 'warning', and the value of 'reason'
+    is (should be) a human readable string explaining the issue.
+    """
+
+    issue_types = ['error', 'warning']
+
+    def __init__(self, allow_duplicates=False):
+        """
+        If allow_duplicates is True issues identical to an already tracked issue
+        will be added, default is to discard duplicates.
+        """
+        super(IssueTracker, self).__init__()
+        self.allow_duplicates = allow_duplicates
+        self._issues = []
+        self._err_count = 0
+        self._warn_count = 0
+        self._default_format = "{type!c}: {reason}"
+
+    def _add_issue(self, issue_type, reason, info):
+        if issue_type not in self.issue_types:
+            raise Exception("issue_type should be one of {}".format(str(self.issue_types)))
+
+        issue = {
+            'type': issue_type,
+            'reason': reason,
+        }
+        if type(info) is dict:
+            issue.update(info)
+        elif hasattr(info, 'debug_info'):
+            issue.update(info.debug_info or {})
+
+        if self.allow_duplicates or issue not in self._issues:
+            self._issues.append(issue)
+            if issue['type'] == 'error':
+                self._err_count += 1
+            else:
+                self._warn_count += 1
+
+    def add_error(self, reason, info=None):
+        """
+        Add an error.
+
+        Mandatory argument reason should be a human readable string.
+        Optional argument info is a dictionary with information like line number etc.
+        """
+        self._add_issue('error', reason, info)
+
+    def add_warning(self, reason, info=None):
+        """
+        Add an error.
+
+        Mandatory argument reason should be a human readable string.
+        Optional argument info is a dictionary with information like line number etc.
+        """
+        self._add_issue('warning', reason, info)
+
+    @property
+    def error_count(self):
+        """Number of errors tracked"""
+        return self._err_count
+
+    @property
+    def warning_count(self):
+        """Number of warnings tracked"""
+        return self._warn_count
+
+    @property
+    def issue_count(self):
+        """Total number of issues tracked"""
+        return self._warn_count + self._err_count
+
+    def issues(self, issue_type=None, sort_key=None):
+        """
+        Return a list of issues.
+
+        If issue_type is given, only return issues of that type.
+        If sort_key is given, use that to retrieve a value from each issue
+        used to sort the result. If sort_key is not present in issue, sort
+        order is undefined.
+        """
+        if issue_type:
+            issues = [i for i in self._issues if i['type'] == issue_type]
+        else:
+            issues = self._issues[:]
+        if sort_key:
+            # Sort in place since we have a copy
+            # If sort_key not in issue(s) order is undefined
+            # Note: Won't work on python3
+            issues.sort(key=lambda k: k.get(sort_key))
+        return issues
+
+    def errors(self, sort_key=None):
+        """
+        Return a list of errors.
+
+        If sort_key is given, use that to retrieve a value from each error
+        used to sort the result. If sort_key is not present in error, sort
+        order is undefined.
+        """
+        return self.issues(issue_type='error', sort_key=sort_key)
+        # return [e for e in self._issues if e['type'] == 'error']
+
+    def warnings(self, sort_key=None):
+        """
+        Return a list of warnings.
+
+        If sort_key is given, use that to retrieve a value from each warning
+        used to sort the result. If sort_key is not present in warning, sort
+        order is undefined.
+        """
+        return self.issues(issue_type='warning', sort_key=sort_key)
+        # return [e for e in self._issues if e['type'] == 'warning']
+
+    def _format_items(self, items, item_format, **kwargs):
+        if not item_format:
+            item_format = self._default_format
+        did_warn = False
+        fmt = IssueFormatter()
+        result = []
+        for item in items:
+            combined = {}
+            combined.update(kwargs)
+            combined.update(item)
+            try:
+                x = fmt.format(item_format, **combined)
+            except:
+                # This should not fail
+                if not did_warn:
+                    sys.stderr.write("Bad format string '{}', using default.\n".format(str(item_format)))
+                    sys.stderr.write("Available keys: {}\n".format(str(combined.keys())))
+                    did_warn = True
+                x = fmt.format(self._default_format, **combined)
+
+            result.append(x)
+
+        return result
+
+    def formatted_issues(self, issue_type=None, sort_key=None, custom_format=None, **kwargs):
+        """
+        Return a list of issues formatted as strings.
+
+        Optional parameters issue_type and sort_key behaves as for issues().
+        If custom_format is given it should follow string.format() rules, with the
+        additional string conversion options 'u', 'l', and 'c' for upper, lower,
+        and capitalize, respectively, e.g. "{foo!u}" for uppercasing. The default format is
+        "{type!c}: {reason}", which is also the fallback format in case of any errors.
+        Any additional key-value arguments will be available to the formatter, e.g.
+        custom_format="{type!c}: {reason} {filename}:{line}", filename="foo/bar.calvin".
+        Values from the actual issue will take precedence over additional values for the same key.
+        """
+        return self._format_items(self.issues(sort_key=sort_key), custom_format, **kwargs)
+
+    def formatted_errors(self, sort_key=None, custom_format=None, **kwargs):
+        """
+        Return a list of errors formatted as strings.
+
+        Optional parameter sort_key behaves as for issues(). Custom_format and extra
+        key-value arguments are explained in formatted_issues().
+        """
+        return self._format_items(self.errors(sort_key=sort_key), custom_format, **kwargs)
+
+    def formatted_warnings(self, sort_key=None, custom_format=None, **kwargs):
+        """
+        Return a list of warnings formatted as strings.
+
+        Optional parameter sort_key behaves as for issues(). Custom_format and extra
+        key-value arguments are explained in formatted_issues().
+        """
+        return self._format_items(self.warnings(sort_key=sort_key), custom_format, **kwargs)
+
+
+if __name__ == '__main__':
+
+    myfmt = IssueFormatter()
+    print myfmt.format("{foo!u} {foo!c}", foo="hello")
+    print myfmt.format("{0!u} {0!c}", "hello")
+    print myfmt.format("{!u} {!c}", "hello", "again")
+
+    t = IssueTracker()
+    t.add_error("Foo")
+    t.add_warning("Bar")
+
+    print t.issues()
+    print t.error_count, t.errors()
+    print t.warning_count, t.warnings()
+    print t.issue_count, t.issues()
+    t.add_warning("Bar")
+    assert t.warning_count == 1
+    t.allow_duplicates = True
+    t.add_warning("Bar")
+    assert t.warning_count == 2
+
+    for f in t.formatted_issues():
+        print f
+
+    for f in t.formatted_warnings():
+        print f
+
+    for f in t.formatted_errors():
+        print f
+
+    for f in t.formatted_issues(custom_format="{reason}"):
+        print f
+
+    for f in t.formatted_warnings(custom_format="{type!u} - {reason}"):
+        print f
+
+    for f in t.formatted_errors(custom_format="{type} - {reason!u}"):
+        print f
+
+    for f in t.formatted_issues(custom_format="{no_reason}"):
+        print f
+
+    t.add_error("Line", {'line':0, 'col':0, 'extra':42})
+
+    for f in t.formatted_issues(custom_format="{no_reason}"):
+        print f
+
+    t.add_error("Apa")
+    t.add_error("Ara")
+    print "--- Sorted (type) ---"
+    for f in t.formatted_issues(custom_format="{no_reason}", sort_key="type"):
+        print f
+    print "--- Sorted (reason) ---"
+    for f in t.formatted_issues(custom_format="{no_reason}", sort_key="reason"):
+        print f
+    print "--- Sorted (BAD: no_reason) ---"
+    for f in t.formatted_issues(sort_key="no_reason"):
+        print f
+    print "--- Sorted (line) ---"
+    for f in t.formatted_errors(custom_format="{type!c}: {reason} {line}:{col}", sort_key="line"):
+        print f
+
+    print "--- Sorted (extras) ---"
+    for f in t.formatted_errors(custom_format="{type!c}: {reason} {filename}, line: {line}", sort_key="line", filename="baz.calvin", line="bogus"):
+        print f
+
+
+
+
+
