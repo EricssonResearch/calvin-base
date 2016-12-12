9de2ea600eeda809787e7c7b2eb28810969fd0b7
Ola Angelsmark <ola.angelsmark@ericsson.com>
2016-12-12 16:01:14 +0100

    Environmental: Add fake implementation

---
+++ calvin/runtime/south/plugins/io/sensors/environmental/random_impl/environmental.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2016 Ericsson AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from calvin.runtime.south.plugins.io.sensors.environmental import base_environmental
+import random
+
+
+
+
+class Environmental(base_environmental.EnvironmentalBase):
+
+    """
+    Faked implementation of Environmental
+    """
+
+
+    def get_temperature(self):
+        return int(random.uniform(21, 22)*10)/10.0
+
+    def get_humidity(self):
+        return int(random.uniform(60, 65)*5)/5.0
+
+    def get_pressure(self):
+        return int(random.uniform(1.01, 1.02)*100)/100.0
