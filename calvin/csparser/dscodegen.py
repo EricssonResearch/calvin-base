ae1f3df2c88379a22fdfa1125e65350a87569ad4
Per Persson <per.persson@ericsson.com>
2017-05-04 17:10:18 +0200

    Simplest case handled

--- calvin/csparser/dscodegen.py
+++ calvin/csparser/dscodegen.py
@@ -2,13 +2,19 @@ import astnode as ast
 import visitor
 import astprint
 from parser import calvin_parse
+from codegen import query
 
-class Finder(object):
-    """
-    Perform queries on the tree
-    """
-    def __init__(self):
-        super(Finder, self).__init__()
+
+class RuleExpander(object):
+    """docstring for RuleExpander"""
+    def __init__(self, root, issue_tracker):
+        super(RuleExpander, self).__init__()
+        self.root = root
+        self.issue_tracker = issue_tracker
+
+    def process(self):
+        self.result = []
+        self.visit(self.root)
 
     @visitor.on('node')
     def visit(self, node):
@@ -16,37 +22,30 @@ class Finder(object):
 
     @visitor.when(ast.Node)
     def visit(self, node):
-        if node.matches(self.kind, self.attributes):
-            self.matches.append(node)
-        if not node.is_leaf() and self.depth < self.maxdepth:
-            self.depth += 1
+        if not node.is_leaf():
             map(self.visit, node.children)
-            self.depth -= 1
 
-    def find_all(self, root, kind=None, attributes=None, maxdepth=1024):
-        """
-        Return a list of all nodes matching <kind>, at most <maxdepth> levels
-        down from the starting node <node>
-        """
-        self.depth = 0
-        self.kind = kind
-        self.maxdepth = maxdepth
-        self.matches = []
-        self.attributes = attributes
-        self.visit(root)
-        return self.matches
+    @visitor.when(ast.Rule)
+    def visit(self, node):
+        print "rule", node
+
+    @visitor.when(ast.RulePredicate)
+    def visit(self, node):
+        # print "predicate", node
+        pred = {c.ident.ident:c.arg.value for c in node.children}
+        res = {"kwargs":pred, "type":"+", "op":node.predicate.ident}
+        self.result.append(res)
+
 
 class DeployInfo(object):
     """docstring for DeployInfo"""
-    def __init__(self, deploy_info, root, issue_tracker, known_actors=None):
+    def __init__(self, root, issue_tracker):
         super(DeployInfo, self).__init__()
         self.root = root
-        self.deploy_info = deploy_info
         self.issue_tracker = issue_tracker
-        self.current_target = None
-        self.stacked_target = None
 
     def process(self):
+        self.requirements = {}
         self.visit(self.root)
 
     @visitor.on('node')
@@ -60,114 +59,19 @@ class DeployInfo(object):
 
     @visitor.when(ast.RuleApply)
     def visit(self, node):
-        for target in node.children:
-            actor_name = target.ident
-            self.deploy_info['requirements'].setdefault(actor_name, [])
-            self.current_target = self.deploy_info['requirements'][actor_name]
-            if not node.rule.is_leaf():
-                map(self.visit, node.rule.children)
-            self.current_target = None
+        rule_name = node.rule.ident
+        matched = query(self.root, kind=ast.RuleDefinition, attributes={('name', 'ident'):rule_name}, maxdepth=1024)
+        # print matched, rule_name
+        rule_def = matched[0]
+        expr = expand_rule(rule_def.rule, self.issue_tracker)
+        for c in node.children:
+            self.requirements[c.ident] = expr
 
-    @visitor.when(ast.RulePredicate)
-    def visit(self, node):
-        if self.current_target is None:
-            return
-        value = {}
-        if "|" in node.op.op and not self.stacked_target:
-            union_group = {'op': "union_group", 'type': '+', 'requirements': []}
-            self.current_target.append(union_group)
-            self.stacked_target = self.current_target
-            self.current_target = union_group['requirements']
-        value['type'] = "-" if "~" in node.op.op else "+"
-        value['op'] = node.predicate.ident
-        value['kwargs'] = {a.ident.ident: a.arg.ident if isinstance(a.arg, ast.Id) else a.arg.value
-                            for a in node.children}
-        self.current_target.append(value)
-        # FIXME We don't handle mixing union and intersection in same expression
-        if node.next_sibling() is None:
-            self.current_target = self.stacked_target
-            self.stacked_target = None
-
-class FoldInRuleExpression(object):
-    """docstring for FoldInRuleExpression"""
-    def __init__(self, issue_tracker):
-        super(FoldInRuleExpression, self).__init__()
-        self.issue_tracker = issue_tracker
-
-    def process(self, root):
-        self.root = root
-        self.visit(root)
-
-    @visitor.on('node')
-    def visit(self, node):
-        pass
-
-    @visitor.when(ast.Node)
-    def visit(self, node):
-        if not node.is_leaf():
-            map(self.visit, node.children)
-
-    @visitor.when(ast.RuleApply)
-    def visit(self, node):
-        if not node.rule.is_leaf():
-            map(self.visit, node.rule.children)
-
-    @visitor.when(ast.RulePredicate)
-    def visit(self, node):
-        if node.type != "rule":
-            return
-        rules = query(self.root, kind=ast.Rule, attributes={('rule', 'ident'): node.predicate.ident})
-        if not rules:
-            reason = "Refers to undefined rule {}".format(node.predicate.ident)
-            self.issue_tracker.add_error(reason, node)
-            return
-        # There should only be one rule with this ident and it should only have one child
-        clone = rules[0].children[0].clone()
-        node.parent.replace_child(node, clone)
-        del node
-        # Make sure that the clone is visited
-        if not clone.is_leaf():
-            map(self.visit, clone.children)
-
-class SetOpOnFirstPredicate(object):
-    """docstring for SetOpOnFirstPredicate"""
-    def __init__(self, issue_tracker):
-        super(SetOpOnFirstPredicate, self).__init__()
-        self.issue_tracker = issue_tracker
-
-    def process(self, root):
-        self.root = root
-        self.visit(root)
-
-    @visitor.on('node')
-    def visit(self, node):
-        pass
-
-    @visitor.when(ast.Node)
-    def visit(self, node):
-        if not node.is_leaf():
-            map(self.visit, node.children)
-
-    @visitor.when(ast.RuleApply)
-    def visit(self, node):
-        if not node.rule.is_leaf():
-            map(self.visit, node.rule.children)
-
-    @visitor.when(ast.RulePredicate)
-    def visit(self, node):
-        if len(node.op.op) == 0 or (len(node.op.op) == 1 and node.op.op == "~"):
-            next_predicate = node.next_sibling()
-            if next_predicate is None:
-                # Alone in the expression set & anyway
-                node.op.op = "&" + node.op.op
-            else:
-                # Make the op match the next predicate (besides any ~ operator)
-                node.op.op = next_predicate.op.op[0] + node.op.op
 
 class DSCodeGen(object):
 
-    verbose = False
-    verbose_nodes = False
+    verbose = True
+    verbose_nodes = True
 
     """
     Generate code from a deploy script file
@@ -175,12 +79,7 @@ class DSCodeGen(object):
     def __init__(self, ast_root, script_name):
         super(DSCodeGen, self).__init__()
         self.root = ast_root
-        self.deploy_info = {
-            'requirements':{},
-            'valid': True
-        }
         self.dump_tree('ROOT')
-
 
     def dump_tree(self, heading):
         if not self.verbose:
@@ -190,33 +89,24 @@ class DSCodeGen(object):
         print "========\n{}\n========".format(heading)
         printer.process(self.root)
 
-    def set_op_on_first_predicates(self, issue_tracker):
-        f = SetOpOnFirstPredicate(issue_tracker)
-        f.process(self.root)
-        self.dump_tree('Set Op On First Predicate')
-
-    def fold_in_rule_expr(self, issue_tracker):
-        f = FoldInRuleExpression(issue_tracker)
-        f.process(self.root)
-        self.dump_tree('Fold In Rule Expression')
 
     def generate_code_from_ast(self, issue_tracker):
-        gen_deploy_info = DeployInfo(self.deploy_info, self.root, issue_tracker)
+        gen_deploy_info = DeployInfo(self.root, issue_tracker)
         gen_deploy_info.process()
+        return gen_deploy_info.requirements
 
     def generate_code(self, issue_tracker):
-        self.set_op_on_first_predicates(issue_tracker)
-        self.fold_in_rule_expr(issue_tracker)
-        self.generate_code_from_ast(issue_tracker)
+        # self.set_op_on_first_predicates(issue_tracker)
+        # self.fold_in_rule_expr(issue_tracker)
+        requirements = self.generate_code_from_ast(issue_tracker)
+        self.deploy_info = {'requirements':requirements}
         self.deploy_info['valid'] = (issue_tracker.error_count == 0)
 
 
-def query(root, kind=None, attributes=None, maxdepth=1024):
-    finder = Finder()
-    finder.find_all(root, kind, attributes=attributes, maxdepth=maxdepth)
-    # print
-    # print "QUERY", kind.__name__, attributes, finder.matches
-    return finder.matches
+def expand_rule(node, issue_tracker):
+    rule_expander = RuleExpander(node, issue_tracker)
+    rule_expander.process()
+    return rule_expander.result
 
 def _calvin_cg(source_text, app_name):
     ast_root, issuetracker = calvin_parse(source_text)
