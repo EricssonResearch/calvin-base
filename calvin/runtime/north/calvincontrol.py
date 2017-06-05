9a5f8fcdc025277a7eef968c2e25c397b05e208a
Per Persson <per.persson@ericsson.com>
2017-06-05 14:41:05 +0200

    Moving out security API

--- calvin/runtime/north/calvincontrol.py
+++ calvin/runtime/north/calvincontrol.py
@@ -36,6 +36,7 @@ from calvin.utilities.issuetracker import IssueTracker
 # Dynamically build selected set of APIs
 #
 from control_apis import routes
+from control_apis import security_api
 from control_apis import documentation_api
 from control_apis import logging_api
 from control_apis import metering_api
@@ -360,142 +361,6 @@ re_delete_node = re.compile(r"DELETE /node(?:/(now|migrate|clean))?\sHTTP/1")
 
 control_api_doc += \
     """
-    POST /certiticate_authority/certificate_signing_request
-    Send CSR to CA, that creates a x509 certificate and returns it
-    Response status code: OK or INTERNAL_ERROR
-    Response:
-    {"certificate":<value>}
-"""
-re_post_certificate_signing_request = re.compile(r"POST /certificate_authority/certificate_signing_request\sHTTP/1")
-
-control_api_doc += \
-    """
-    PUT /certiticate_authority/certificate_enrollment_password/{node_name}
-    Set a password to be later user as authorization for the Certificate Signing Request from the runtime
-    Body:
-    {
-        "value": <string>
-    }
-    Response status code: OK or INTERNAL_ERROR
-    Response: none
-"""
-re_edit_certificate_enrollment_password = re.compile(r"PUT /certificate_authority/certificate_enrollment_password/([0-9a-zA-Z\.\-/_]*)\sHTTP/1")
-
-control_api_doc += \
-    """
-    GET /certiticate_authority/certificate_enrollment_password/{node_name}
-    Request a password to be later user as authorization for the Certificate Signing Request from the runtime
-    Response status code: OK or INTERNAL_ERROR
-    Response:
-    {"enrollment_password":<value>}
-"""
-re_get_certificate_enrollment_password = re.compile(r"GET /certificate_authority/certificate_enrollment_password/([0-9a-zA-Z\.\-/_]*)\sHTTP/1")
-
-control_api_doc += \
-    """
-    GET /authentication/users_db
-    Get user database on this runtime
-    Response status code: OK or INTERNAL_ERROR
-    Response:
-    {
-        "policies": {
-            <policy-id>: policy in JSON format,
-            ...
-        }
-    }
-"""
-re_get_authentication_users_db = re.compile(r"GET /authentication/users_db\sHTTP/1")
-
-control_api_doc += \
-    """
-    PUT /authentication/users_db
-    Update user database
-    Body: new policy in JSON format
-    Response status code: OK, INTERNAL_ERROR or NOT_FOUND
-    Response: none
-"""
-re_edit_authentication_users_db = re.compile(r"PUT /authentication/users_db\sHTTP/1")
-
-control_api_doc += \
-    """
-    GET /authentication/groups_db
-    Get user database on this runtime
-    Response status code: OK or INTERNAL_ERROR
-    Response:
-    {
-        "policies": {
-            <policy-id>: policy in JSON format,
-            ...
-        }
-    }
-"""
-re_get_authentication_groups_db = re.compile(r"GET /authentication/groups_db\sHTTP/1")
-
-control_api_doc += \
-    """
-    PUT /authentication/groups_db
-    Update user database
-    Body: new policy in JSON format
-    Response status code: OK, INTERNAL_ERROR or NOT_FOUND
-    Response: none
-"""
-re_edit_authentication_groups_db = re.compile(r"PUT /authentication/groups_db\sHTTP/1")
-
-control_api_doc += \
-    """
-    POST /authorization/policies
-    Create a new policy
-    Body: policy in JSON format
-    Response status code: OK or INTERNAL_ERROR
-    Response: {"policy_id": <policy-id>}
-"""
-re_post_new_authorization_policy = re.compile(r"POST /authorization/policies\sHTTP/1")
-
-control_api_doc += \
-    """
-    GET /authorization/policies
-    Get all policies on this runtime
-    Response status code: OK or INTERNAL_ERROR
-    Response:
-    {
-        "policies": {
-            <policy-id>: policy in JSON format,
-            ...
-        }
-    }
-"""
-re_get_authorization_policies = re.compile(r"GET /authorization/policies\sHTTP/1")
-
-control_api_doc += \
-    """
-    GET /authorization/policies/{policy-id}
-    Get policy
-    Response status code: OK, INTERNAL_ERROR or NOT_FOUND
-    Response: {"policy": policy in JSON format}
-"""
-re_get_authorization_policy = re.compile(r"GET /authorization/policies/(POLICY_" + uuid_re + "|" + uuid_re + ")\sHTTP/1")
-
-control_api_doc += \
-    """
-    PUT /authorization/policies/{policy-id}
-    Update policy
-    Body: new policy in JSON format
-    Response status code: OK, INTERNAL_ERROR or NOT_FOUND
-    Response: none
-"""
-re_edit_authorization_policy = re.compile(r"PUT /authorization/policies/(POLICY_" + uuid_re + "|" + uuid_re + ")\sHTTP/1")
-
-control_api_doc += \
-    """
-    DELETE /authorization/policies/{policy-id}
-    Delete policy
-    Response status code: OK, INTERNAL_ERROR or NOT_FOUND
-    Response: none
-"""
-re_del_authorization_policy = re.compile(r"DELETE /authorization/policies/(POLICY_" + uuid_re + "|" + uuid_re + ")\sHTTP/1")
-
-control_api_doc += \
-    """
     OPTIONS /url
     Request for information about the communication options available on url
     Response status code: OK
@@ -577,18 +442,18 @@ class CalvinControl(object):
             # (re_get_storage, self.handle_get_storage),
             # (re_dump_storage, self.handle_dump_storage),
             # (re_post_storage, self.handle_post_storage),
-            (re_post_certificate_signing_request,self.handle_post_certificate_signing_request),
-            (re_edit_certificate_enrollment_password, self.handle_edit_certificate_enrollment_password),
-            (re_get_certificate_enrollment_password, self.handle_get_certificate_enrollment_password),
-            (re_get_authentication_users_db, self.handle_get_authentication_users_db),
-            (re_edit_authentication_users_db, self.handle_edit_authentication_users_db),
-            (re_get_authentication_groups_db, self.handle_get_authentication_groups_db),
-            (re_edit_authentication_groups_db, self.handle_edit_authentication_groups_db),
-            (re_post_new_authorization_policy, self.handle_new_authorization_policy),
-            (re_get_authorization_policies, self.handle_get_authorization_policies),
-            (re_get_authorization_policy, self.handle_get_authorization_policy),
-            (re_edit_authorization_policy, self.handle_edit_authorization_policy),
-            (re_del_authorization_policy, self.handle_del_authorization_policy),
+            # (re_post_certificate_signing_request,self.handle_post_certificate_signing_request),
+            # (re_edit_certificate_enrollment_password, self.handle_edit_certificate_enrollment_password),
+            # (re_get_certificate_enrollment_password, self.handle_get_certificate_enrollment_password),
+            # (re_get_authentication_users_db, self.handle_get_authentication_users_db),
+            # (re_edit_authentication_users_db, self.handle_edit_authentication_users_db),
+            # (re_get_authentication_groups_db, self.handle_get_authentication_groups_db),
+            # (re_edit_authentication_groups_db, self.handle_edit_authentication_groups_db),
+            # (re_post_new_authorization_policy, self.handle_new_authorization_policy),
+            # (re_get_authorization_policies, self.handle_get_authorization_policies),
+            # (re_get_authorization_policy, self.handle_get_authorization_policy),
+            # (re_edit_authorization_policy, self.handle_edit_authorization_policy),
+            # (re_del_authorization_policy, self.handle_del_authorization_policy),
             (re_options, self.handle_options)
         ]
 
@@ -999,6 +864,8 @@ class CalvinControl(object):
     def handle_connect(self, handle, connection, match, data, hdr):
         """ Connect port
         """
+        # FIXME: The long and winding construct should be replaced by the more concise:
+        #        self.node.connect(**data, cb=CalvinCB(self.handle_connect_cb, handle, connection))
         self.node.connect(
             actor_id=data.get("actor_id"),
             port_name=data.get("port_name"),
@@ -1210,157 +1077,6 @@ class CalvinControl(object):
         _log.analyze(self.node.id, "+ DISCONNECTED", {'status': status.status}, tb=True)
         self.send_response(handle, connection, None, status=status.status)
 
-    # No authentication decorator, this is called by the runtimes when deployed
-    # without a certificate
-    def handle_post_certificate_signing_request(self, handle, connection, match, data, hdr):
-        """Post a CSR, if ok, CA creates and returns X509 certificate """
-        try:
-            cert = self.node.certificate_authority.sign_csr_request(data)
-            status = calvinresponse.OK
-        except:
-            _log.exception("handle_post_certificate_signing_request")
-            status = calvinresponse.INTERNAL_ERROR
-        self.send_response(handle, connection, json.dumps({"certificate": cert}) if status == calvinresponse.OK else None,
-                           status=status)
-
-    #Only authorized users, e.g.,an admin, should be allowed to query certificate enrollment passwords
-    # from the CA runtime
-    @authentication_decorator
-    def handle_get_certificate_enrollment_password(self, handle, connection, match, data, hdr):
-        """Get a challenge password for node_name """
-        try:
-            password = self.node.certificate_authority.get_enrollment_password(node_name=match.group(1))
-            status = calvinresponse.OK
-        except:
-            _log.exception("handle_post_certificate_enrollment_password")
-            status = calvinresponse.INTERNAL_ERROR
-        self.send_response(handle, connection, json.dumps({"enrollment_password": password}) if status == calvinresponse.OK else None,
-                           status=status)
-
-    #Only authorized users, e.g.,an admin, should be allowed to query certificate enrollment passwords
-    # from the CA runtime
-    @authentication_decorator
-    def handle_edit_certificate_enrollment_password(self, handle, connection, match, data, hdr):
-        """Post a challenge password for node_name"""
-        try:
-            password = self.node.certificate_authority.set_enrollment_password(node_name=match.group(1), password=data['enrollment_password'])
-            status = calvinresponse.OK
-        except:
-            _log.exception("handle_post_certificate_enrollment_password")
-            status = calvinresponse.INTERNAL_ERROR
-        self.send_response(handle, connection, None, status=status)
-
-    @authentication_decorator
-    def handle_get_authentication_users_db(self, handle, connection, match, data, hdr):
-        """Get all authorization policies on this runtime"""
-        try:
-            users_db = self.node.authentication.arp.get_users_db()
-            status = calvinresponse.OK
-        except:
-            _log.exception("handle_get_authentication_users_db")
-            status = calvinresponse.INTERNAL_ERROR
-        self.send_response(handle, connection, json.dumps({"users_db": users_db}) if status == calvinresponse.OK else None,
-                           status=status)
-
-    @authentication_decorator
-    def handle_edit_authentication_users_db(self, handle, connection, match, data, hdr):
-        """Edit users database"""
-        if 'users_db'in data:
-            try:
-    #            self.node.authentication.arp.update_users_db(data, match.group(1))
-                self.node.authentication.arp.update_users_db(data['users_db'])
-                status = calvinresponse.OK
-            except IOError as err:
-                _log.exception("handle_edit_authentication_users_db, err={}".format(err))
-                status = calvinresponse.NOT_FOUND
-            except Exception as err:
-                _log.exception("handle_edit_authentication_users_db, err={}".format(err))
-                status = calvinresponse.INTERNAL_ERROR
-        else:
-            _log.exception("handle_edit_authentication_users_db: no users_db in data\n\tdata={}".format(data))
-            status = calvinresponse.NOT_FOUND
-        self.send_response(handle, connection, None, status=status)
-
-    @authentication_decorator
-    def handle_get_authentication_groups_db(self, handle, connection, match, data, hdr):
-        """Get all authorization policies on this runtime"""
-        #TODO:to be implemented
-
-    @authentication_decorator
-    def handle_edit_authentication_groups_db(self, handle, connection, match, data, hdr):
-        """Edit authorization policy identified by id"""
-        #TODO:to be implemented
-
-    @authentication_decorator
-    def handle_new_authorization_policy(self, handle, connection, match, data, hdr):
-        """Create authorization policy"""
-        # TODO: need some kind of authentication for policy management
-        try:
-            policy_id = self.node.authorization.prp.create_policy(data)
-            status = calvinresponse.OK
-        except:
-            policy_id = None
-            _log.exception("handle_new_authorization_policy")
-            status = calvinresponse.INTERNAL_ERROR
-        self.send_response(handle, connection, None if policy_id is None else json.dumps({'policy_id': policy_id}),
-                           status=status)
-
-    @authentication_decorator
-    def handle_get_authorization_policies(self, handle, connection, match, data, hdr):
-        """Get all authorization policies on this runtime"""
-        try:
-            policies = self.node.authorization.prp.get_policies()
-            status = calvinresponse.OK
-        except:
-            _log.exception("handle_get_authorization_policies")
-            status = calvinresponse.INTERNAL_ERROR
-        self.send_response(handle, connection, json.dumps({"policies": policies}) if status == calvinresponse.OK else None,
-                           status=status)
-
-    @authentication_decorator
-    def handle_get_authorization_policy(self, handle, connection, match, data, hdr):
-        """Get authorization policy identified by id"""
-        try:
-            data = self.node.authorization.prp.get_policy(match.group(1))
-            status = calvinresponse.OK
-        except IOError:
-            _log.exception("handle_get_authorization_policy")
-            status = calvinresponse.NOT_FOUND
-        except:
-            _log.exception("handle_get_authorization_policy")
-            status = calvinresponse.INTERNAL_ERROR
-        self.send_response(handle, connection, json.dumps({"policy": data}) if status == calvinresponse.OK else None,
-                           status=status)
-
-    @authentication_decorator
-    def handle_edit_authorization_policy(self, handle, connection, match, data, hdr):
-        """Edit authorization policy identified by id"""
-        # TODO: need some kind of authentication for policy management
-        try:
-            self.node.authorization.prp.update_policy(data, match.group(1))
-            status = calvinresponse.OK
-        except IOError:
-            _log.exception("handle_edit_authorization_policy")
-            status = calvinresponse.NOT_FOUND
-        except:
-            _log.exception("handle_edit_authorization_policy")
-            status = calvinresponse.INTERNAL_ERROR
-        self.send_response(handle, connection, None, status=status)
-
-    @authentication_decorator
-    def handle_del_authorization_policy(self, handle, connection, match, data, hdr):
-        """ Delete authorization policy identified by id"""
-        # TODO: need some kind of authentication for policy management
-        try:
-            self.node.authorization.prp.delete_policy(match.group(1))
-            status = calvinresponse.OK
-        except OSError:
-            _log.exception("handle_del_authorization_policy")
-            status = calvinresponse.NOT_FOUND
-        except:
-            _log.exception("handle_del_authorization_policy")
-            status = calvinresponse.INTERNAL_ERROR
-        self.send_response(handle, connection, None, status=status)
 
     #
     # Logging hooks
