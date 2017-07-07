ce63758da652af56f33cb5c5fb30207a56599de7
Ola Angelsmark <ola.angelsmark@ericsson.com>
2017-07-07 11:22:53 +0200

    Actor: Add generic DigitalIn/DigitalOut

---
+++ calvin/actorstore/systemactors/io/DigitalIn.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2017 Ericsson AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from calvin.actor.actor import Actor, manage, condition, stateguard, calvinsys
+
+
+class DigitalIn(Actor):
+
+    """
+    Edge triggered digital input in runtime-defined pin.
+    Outputs:
+        state: 1/0 when edge goes high/low
+    """
+
+    @manage()
+    def init(self):
+        self._pin = None
+        self.setup()
+
+    def setup(self):
+        self._pin = calvinsys.open(self, "io.digitalin")
+
+    def will_migrate(self):
+        if self._pin:
+            calvinsys.close(self._pin)
+        self._pin = None
+
+    def will_end(self):
+        if self._pin :
+            calvinsys.close(self._pin)
+
+    def did_migrate(self):
+        self.setup()
+
+    @stateguard(lambda self: self._pin and calvinsys.can_read(self._pin))
+    @condition([], ["state"])
+    def read_pin(self):
+        state = calvinsys.read(self._pin)
+        return (1 if state else 0,)
+
+    action_priority = (read_pin, )
+    requires = ["io.digitalin"]
---
+++ calvin/actorstore/systemactors/io/DigitalOut.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2017 Ericsson AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from calvin.actor.actor import Actor, manage, condition, stateguard, calvinsys
+
+
+class DigitalOut(Actor):
+
+    """
+    Set runtime defined-pin to inport state.
+    Inputs:
+        state: 1/0
+    """
+
+    @manage()
+    def init(self):
+        self._pin = None
+        self.setup()
+
+    def setup(self):
+        self._pin = calvinsys.open(self, "io.digitalout")
+
+    def will_migrate(self):
+        if self._pin:
+            calvinsys.close(self._pin)
+        self._pin = None
+
+    def will_end(self):
+        if self._pin :
+            calvinsys.close(self._pin)
+
+    def did_migrate(self):
+        self.setup()
+
+    @stateguard(lambda self: self._pin and calvinsys.can_write(self._pin))
+    @condition(["state"], [])
+    def write_pin(self, state):
+        calvinsys.write(self._pin, 1 if state else 0)
+
+
+    action_priority = (write_pin, )
+    requires = ["io.digitalout"]
