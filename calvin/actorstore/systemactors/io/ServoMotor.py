54487c4ec2ed6db5567f21eaabca6f8568dd9b6b
Ola Angelsmark <ola.angelsmark@ericsson.com>
2017-09-05 12:40:39 +0200

    Servo: Now includes actor implementation

---
+++ calvin/actorstore/systemactors/io/ServoMotor.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015 Ericsson AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from calvin.actor.actor import Actor, condition, manage, calvinsys
+from calvin.utilities.calvinlogger import get_actor_logger
+
+_log = get_actor_logger(__name__)
+
+class ServoMotor(Actor):
+
+    """
+    Rotate servo given degrees.
+    Input:
+      angle : set servo to given angle
+    """
+
+    @manage([])
+    def init(self):
+        _log.info("init")
+        self.setup()
+
+    def setup(self):
+        self._servo = calvinsys.open(self, "io.servomotor")
+        calvinsys.write(self._servo, 90)
+
+    @condition(action_input=("angle",))
+    def set_angle(self, angle):
+        calvinsys.write(self._servo, angle)
+
+    action_priority = (set_angle, )
+    requires = ["io.servomotor"]
