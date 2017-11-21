de74576f8e9d6c9ed20a40283770e5bf45fa5e42
Fredrik Svensson <fredrik.a.svensson@ericsson.com>
2017-11-21 10:23:36 +0100

    proto: Added handling of migration requests with the peer node set

--- calvin/runtime/north/calvin_proto.py
+++ calvin/runtime/north/calvin_proto.py
@@ -317,7 +317,12 @@ class CalvinProto(CalvinCBClass):
 
     def actor_migrate_handler(self, payload):
         """ Peer request new actor with state and connections """
-        self.node.am.update_requirements(payload['actor_id'], payload['requirements'],
+        if 'peer_node_id' in payload:
+            self.node.am.migrate(payload['actor_id'], payload['peer_node_id'],
+                callback=CalvinCB(self.node.network.link_request, payload['from_rt_uuid'], callback=CalvinCB(send_message,
+                                           msg = {'cmd': 'REPLY', 'msg_uuid': payload['msg_uuid']})))
+        else:
+            self.node.am.update_requirements(payload['actor_id'], payload['requirements'],
                                          payload['extend'], payload['move'],
                                          callback=CalvinCB(self.node.network.link_request, payload['from_rt_uuid'], callback=CalvinCB(send_message,
                                                                     msg = {'cmd': 'REPLY', 'msg_uuid': payload['msg_uuid']})))
