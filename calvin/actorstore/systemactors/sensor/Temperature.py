364de56f207dfe73356cc2d6c4dee583cc581b92
Fredrik Svensson <fredrik.a.svensson@ericsson.com>
2017-11-30 12:52:05 +0100

    sensor.Temperature: Added period to timer state and poll with can_read

--- calvin/actorstore/systemactors/sensor/Temperature.py
+++ calvin/actorstore/systemactors/sensor/Temperature.py
@@ -32,9 +32,9 @@ class Temperature(Actor):
     def init(self, period):
         self.period = period
         self.temperature = calvinsys.open(self, "io.temperature")
-        self.timer = calvinsys.open(self, "sys.timer.once", period=0)
+        self.timer = calvinsys.open(self, "sys.timer.once", period=period)
 
-    @stateguard(lambda self: calvinsys.can_read(self.temperature) and calvinsys.can_write(self.timer))
+    @stateguard(lambda self: calvinsys.can_read(self.temperature) and calvinsys.can_read(self.timer))
     @condition([], ['centigrade'])
     def read_measurement(self):
         value = calvinsys.read(self.temperature)
