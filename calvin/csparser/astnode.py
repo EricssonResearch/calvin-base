687e24dc20856ccaa27553efb94ae4d500c1714a
Per Persson <per.persson@ericsson.com>
2017-12-05 14:28:46 +0100

    This is a bug

--- calvin/csparser/astnode.py
+++ calvin/csparser/astnode.py
@@ -471,7 +471,6 @@ def node_decoder(o):
         'InPort':InPort,
         'OutPort':OutPort,
         'ImplicitPort':ImplicitPort,
-        'InternalPort':InternalPort,
         'InternalInPort':InternalInPort,
         'InternalOutPort':InternalOutPort,
         'Block':Block,
