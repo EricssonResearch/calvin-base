a966ba32298ed0f1175bf91bbd1138b58f07a830
Per Persson <persquare@mac.com>
2019-01-17 10:42:58 +0100

    Breaking out doc generation. With tests.

--- calvin/Tools/calvindoc.py
+++ calvin/Tools/calvindoc.py
@@ -18,45 +18,7 @@
 import json
 import sys
 import argparse
-import requests
-from calvin.actorstore.docobject import DocFormatter
-
-class DocumentationStore(object):
-    """docstring for DocumentationStore"""
-    def __init__(self, host="127.0.0.1", port=4999):
-        super(DocumentationStore, self).__init__()
-        self.host = host
-        self.port = port
-        self.base_request = "http://{}:{}/actors".format(host, port)
-    
-    def _retrieve_metadata(self, what):
-        ns, name = self._format_what(what)
-        if ns or name:
-            req_str = '{}/{}/{}'.format(self.base_request, ns, name)
-        else:
-            req_str = '{}/'.format(self.base_request)
-        r = requests.get(req_str)
-        if r.status_code != 200:
-            raise("BAD STORE")
-        res = r.json()
-        metadata = res['properties']
-        return metadata
-        
-    def _format_what(self, what):
-        parts = what.split('.') + ['', '']
-        return parts[0:2]
-    
-    def documentation(self):
-        pass
-    
-    def help_raw(self, what):
-        return json.dumps(self._retrieve_metadata(what))
-        
-    def help(self, what, compact, formatting):
-        metadata = self._retrieve_metadata(what)
-        df = DocFormatter(outputformat=formatting, compact=compact)
-        return df.format(metadata)        
-             
+from calvin.utilities.docgen import DocumentationStore         
 
 def main():
 
--- calvin/tests/test_actordocs.py
+++ calvin/tests/test_actordocs.py
@@ -1,77 +1,53 @@
 import pytest
 import unittest
 import json
-from calvin.actorstore.store import DocumentationStore
+from calvin.utilities.docgen import DocumentationStore
 
-class TestBase(unittest.TestCase):
 
-    def setUp(self):
-        self.ds = DocumentationStore()
+@pytest.fixture(scope='module')
+def docstore():
+    ds = DocumentationStore()
+    return ds
 
-    def tearDown(self):
-        pass
+def test_sanity(docstore):
+    assert docstore
 
-class SanityCheck(TestBase):
+def test_help_root_arg_none(docstore):
+    actual = docstore.help(what=None, compact=True, formatting='plain')
+    assert "Module: /" == actual[:9]
 
-    def test_sanity(self):
-        self.assertTrue(self.ds)
-        self.assertTrue(self.ds.docs)
+def test_help_root_arg_empty_string(docstore):
+    actual = docstore.help(what="", compact=True, formatting='plain')
+    assert "Module: /" == actual[:9]
 
-class ModuleDocs(TestBase):
+def test_help_raw_root_arg_none(docstore):
+    actual = json.loads(docstore.help_raw(what=None))
+    assert actual['type'] == 'module'
+    assert actual['name'] == '/'
+    assert actual['items']
 
-    def test_help_root_no_args(self):
-        actual = self.ds.help(compact=True)
-        self.assertEqual("Calvin", actual[:6])
+def test_help_raw_root_arg_empty_string(docstore):
+    actual = json.loads(docstore.help_raw(what=""))
+    assert actual['type'] == 'module'
+    assert actual['name'] == '/'
+    assert actual['items']
 
-    def test_help_root_arg_none(self):
-        actual = self.ds.help(what=None, compact=True)
-        self.assertEqual("Calvin", actual[:6])
+def test_help_raw_flow(docstore):
+    actual = json.loads(docstore.help_raw(what='flow'))
+    assert actual['type'] == 'module'
+    assert actual['name'] == 'flow'
+    assert [x for x in actual['items'] if x['name'] == 'Init']
 
-    def test_help_root_arg_empty_string(self):
-        actual = self.ds.help(what="", compact=True)
-        self.assertEqual("Calvin", actual[:6])
+def test_help_raw_flow_init(docstore):
+    actual = json.loads(docstore.help_raw('flow.Init'))
+    assert actual['is_known']
+    assert 'sys.schedule' in actual['requires']
 
-    def test_help_raw_root_no_args(self):
-        actual = json.loads(self.ds.help_raw())
-        self.assertTrue(actual['modules'])
-        self.assertTrue('std' in actual['modules'])
-        self.assertFalse(actual['actors'])
+def test_help_raw_unknown(docstore):
+    actual = json.loads(docstore.help_raw(what='no_such_thing'))
+    assert actual == {}
 
-    def test_help_raw_root_arg_none(self):
-        actual = json.loads(self.ds.help_raw(what=None))
-        self.assertTrue(actual['modules'])
-        self.assertTrue('std' in actual['modules'])
-        self.assertFalse(actual['actors'])
+def test_help_raw_qualified_unknown(docstore):
+    actual = json.loads(docstore.help_raw(what='no.such.thing'))
+    assert actual == {}
 
-    def test_help_raw_root_arg_empty_string(self):
-        actual = json.loads(self.ds.help_raw(what=""))
-        self.assertEqual(actual['short_desc'], 'A systematic approach to handling impedance mismatch in IoT.')
-
-    def test_help_raw_flow(self):
-        actual = json.loads(self.ds.help_raw(what='flow'))
-        self.assertEqual(set(['is_known', 'long_desc', 'short_desc', 'modules', 'actors']), set(actual.keys()))
-        self.assertTrue(actual['actors'])
-        self.assertTrue('Init' in actual['actors'])
-
-    def test_help_raw_flow_init(self):
-        actual = json.loads(self.ds.help_raw('flow.Init'))
-        self.assertTrue(actual['is_known'])
-        self.assertEqual(set(['inputs', 'name', 'outputs', 'args', 'is_known', 'ns', 'type', 'long_desc', 'short_desc', 'output_properties', 'input_properties', 'requires', 'input_docs', 'output_docs']), set(actual.keys()))
-
-    def test_help_raw_unknown(self):
-        actual = json.loads(self.ds.help_raw(what='no_such_thing'))
-        self.assertEqual(actual['short_desc'], 'No such entity')
-
-    def test_help_raw_qualified_unknown(self):
-        actual = json.loads(self.ds.help_raw(what='no.such.thing'))
-        self.assertEqual(actual['short_desc'], 'No such entity')
-
-    def test_metadata_std(self):
-        # For actors only
-        actual = self.ds.metadata('std')
-        self.assertFalse(actual['is_known'])
-
-    def test_metadata_flow_init(self):
-        actual = self.ds.metadata('flow.Init')
-        self.assertTrue(actual['is_known'])
-        self.assertEqual(set(['inputs', 'name', 'outputs', 'args', 'is_known', 'ns', 'type', 'output_properties', 'input_properties', 'requires']), set(actual.keys()))
