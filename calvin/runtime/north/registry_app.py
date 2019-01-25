03bdef22ade69673a9f750165be429faa49e41ab
Per Persson <persquare@mac.com>
2019-01-25 16:30:56 +0100

    Adding more methods

--- calvin/runtime/north/registry_app.py
+++ calvin/runtime/north/registry_app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from storage_clients import LocalRegistry, index_strings 
+from storage_clients import LocalRegistry, index_strings
 from flask import Flask, jsonify, request
 from flask import abort
 
@@ -24,44 +24,56 @@ reg = LocalRegistry()
 def dumpstorage():
     data = reg.dump()
     return jsonify(data)
-    
-@app.route('/storage/<path:prefix_key>', methods=['GET', 'POST'])
-def storage(prefix_key):
+#
+# Plain key/value => /storage/
+#
+@app.route('/storage/<path:key>', methods=['GET', 'POST', 'DELETE'])
+def storage(key):
+    if request.method == 'DELETE':
+        reg.delete(key)
+        return ''
+
     if request.method == 'POST':
         data = request.get_json()
-        if data is None:
-            abort(500)
-        try:
-            reg.set(prefix_key, data['value'])    
-            return ''
-        except:
-            abort(500)
-    # GET    
+        if data is None or 'value' not in data:
+            abort(400)
+        reg.set(key, data['value'])
+        return ''
+
+    # GET
     try:
-        value = reg.get(prefix_key)
+        value = reg.get(key)
         return jsonify({"result":value})
     except:
         abort(404)
-    
+
+#
+# Indexed key/value
+#
 @app.route('/index/<path:key>', methods=['GET', 'POST', 'DELETE'])
 def index(key):
     if request.method in ['POST', 'DELETE']:
         data = request.get_json()
-        if data is None:
-            abort(500)
-        data.get('root_prefix_level', None)
+        if data is None or 'value' not in data:
+            abort(400)
+        root_prefix_level = data.get('root_prefix_level', None)
+        if root_prefix_level is not None:
+            root_prefix_level = int(root_prefix_level)
         indexes = index_strings(key, root_prefix_level)
-        if request.method == 'DELETE':
-            reg.remove_index(indexes, data[value])
-        else:
-            reg.add_index(indexes, data[value])
-        return ''    
+        op = reg.remove_index if request.method == 'DELETE' else reg.add_index
+        try:
+            op(indexes[0], indexes[1:], data['value'])
+        except Exception as e:
+            print "Exception", e
+            abort(400)
+        return ''
+
     # GET
     root_prefix_level = request.args.get('root_prefix_level', None)
     if root_prefix_level is not None:
         root_prefix_level = int(root_prefix_level)
     indexes = index_strings(key, root_prefix_level)
-    value = list(reg.get_index(indexes))
+    value = list(reg.get_index(indexes[0], indexes[1:]))
     return jsonify({"result":value})
 
 
