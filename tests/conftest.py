653be351db248bffcfe41ab6bc42c33894f09fd2
Per Persson <persquare@mac.com>
2019-02-06 16:21:35 +0100

    Adding control API to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,6 +22,7 @@ import time
 
 import pytest
 from mock import Mock
+import requests
 
 from calvin.utilities import calvinuuid
 
@@ -132,6 +133,95 @@ def dummy_node():
 def dummy_peer_node():
     return _DummyNode()         
             
+@pytest.fixture(scope='module')
+def control_api():
+    
+    # PATHS
+    NODE_PATH = '/node/{}'
+    NODE = '/node'
+    NODES = '/nodes'
+    NODE_ID = '/id'
+    ACTOR = '/actor'
+    ACTOR_PATH = '/actor/{}'
+    ACTORS = '/actors'
+    ACTOR_MIGRATE = '/actor/{}/migrate'
+    ACTOR_REPLICATE = '/actor/{}/replicate'
+    APPLICATION_PATH = '/application/{}'
+    APPLICATION_MIGRATE = '/application/{}/migrate'
+    ACTOR_PORT = '/actor/{}/port/{}'
+    ACTOR_REPORT = '/actor/{}/report'
+    APPLICATIONS = '/applications'
+    DEPLOY = '/deploy'
+    INDEX_PATH_RPL = '/index/{}?root_prefix_level={}'
+    INDEX_PATH = '/index/{}'
+    STORAGE_PATH = '/storage/{}'
+
+    class ControlAPI(object):
+        """docstring for ControlAPI"""
+        def __init__(self):
+            super(ControlAPI, self).__init__()
+        
+        # cscontrol, nodecontrol
+        def get_node_id(self, host_uri):
+            response = requests.get(host_uri + NODE_ID)
+            return response.status_code, response.json()
+
+        # cscontrol
+        def deploy(self, host_uri, deployable):
+            response = requests.post(host_uri + DEPLOY, json=deployable)
+            return response.status_code, response.json()
+
+        # # cscontrol, nodecontrol
+        # def get_node(self, rt, node_id):
+        #     response = requests.get(host_uri + NODE_PATH.format(node_id))
+        #     return response.status_code, response.json()
+        #
+        # # cscontrol
+        # def quit(self, rt, method=None):
+        #     if method is None:
+        #         r = self._delete(rt, timeout, async, NODE)
+        #     else:
+        #         r = self._delete(rt, timeout, async, NODE_PATH.format(method))
+        #     return self.check_response(r)
+        #
+        
+        def get_actor(self, host_uri, actor_id):
+            response = requests.get(host_uri + ACTOR_PATH.format(actor_id))
+            return response.status_code, response.json()
+
+        def get_actor_report(self, host_uri, actor_id):
+            response = requests.get(host_uri + ACTOR_REPORT.format(actor_id))
+            return response.status_code, response.json()
+
+        def get_actors(self, host_uri):
+            response = requests.get(host_uri + ACTORS)
+            return response.status_code, response.json()
+
+        # cscontrol
+        def get_applications(self, host_uri):
+            response = requests.get(host_uri + APPLICATIONS)
+            return response.status_code, response.json()
+
+        # cscontrol
+        def get_application(self, host_uri, application_id):
+            response = requests.get(host_uri + APPLICATION_PATH.format(application_id))
+            return response.status_code, response.json()
+
+        # cscontrol
+        def delete_application(self, host_uri, application_id):
+            response = requests.delete(host_uri + APPLICATION_PATH.format(application_id))
+            # FIXME: Make control api at least return consistent type, if not value
+            return response.status_code, None
+        
+            
+        # # cscontrol, utilities.security, utilities.runtime_credentials
+        # def get_index(self, rt, index, root_prefix_level=None):
+        #     if root_prefix_level is None:
+        #         r = self._get(rt, timeout, async, INDEX_PATH.format(index))
+        #     else:
+        #         r = self._get(rt, timeout, async, INDEX_PATH_RPL.format(index, root_prefix_level))
+        #     return self.check_response(r)
+    return ControlAPI()
         
 
 ##################################
