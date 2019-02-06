3ae644593c5272cddcd7a47076fc16eed68f25eb
Per Persson <persquare@mac.com>
2019-02-06 16:20:35 +0100

    Default to local registry

--- calvin/utilities/calvinconfig.py
+++ calvin/utilities/calvinconfig.py
@@ -89,7 +89,7 @@ class CalvinConfig(object):
                 'comment': 'User definable section',
                 'actor_paths': ['systemactors'],
                 'framework': 'twistedimpl',
-                'storage_type': 'dht', # supports dht, securedht, sql, local, and proxy
+                'storage_type': 'local', # supports dht, securedht, sql, local, and proxy
                 'storage_proxy': None,
                 'storage_sql': {},  # For SQL, should have the kwargs to connect + db-name. Defaults to insecure local
                 'capabilities_blacklist': [],
