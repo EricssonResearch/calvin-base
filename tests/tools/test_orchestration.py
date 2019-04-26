c0db88caba9d15f3f33f18462a26966f6836e440
Per Persson <persquare@mac.com>
2019-04-26 11:39:52 +0200

    Minimal test for the newly added local actorstore feature

---
+++ tests/tools/test_orchestration.py
@@ -0,0 +1,18 @@
+import pytest
+import yaml
+
+from calvin.Tools import orchestration
+
+
+def test_actorstore_setup():
+    cfg = """
+    - class: RUNTIME
+      name: rt1
+      actorstore: 
+          uri: local
+    """
+    config = yaml.load(cfg, Loader=yaml.SafeLoader)
+    sm = orchestration.SystemManager(config, start=False)
+    assert sm.info['rt1']['actorstore'] == {'uri': 'local'}
+    
+    
